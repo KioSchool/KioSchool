c89b7f663ecdd217c8247d70af273749022090eb
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-11 17:43:54 +0900

    Add test endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './page/User/Home';
+import AdminHome from './page/Admin/AdminHome';
+import Login from './page/User/Login';
 import WebsocketComponent from './component/WebsocketComponent';
 
 function App() {
   return (
-    <div className="App">
-      <WebsocketComponent workspaceId={38} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/websocket-test" element={<WebsocketComponent workspaceId={38} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
