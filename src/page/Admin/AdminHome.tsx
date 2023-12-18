46caaf0750a45184c850bdd3c0ebed2daa52be83
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-18 23:48:22 +0900

    Navigate to login page

--- src/page/Admin/AdminHome.tsx
+++ src/page/Admin/AdminHome.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { adminApi } from '../../axios';
+import { useNavigate } from 'react-router-dom';
 
 function AdminHome() {
   const [url, setUrl] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    adminApi.get('/user').then((res) => {
-      setUrl(res.data.accountUrl);
-    });
+    adminApi
+      .get('/user')
+      .then((res) => {
+        setUrl(res.data.accountUrl);
+      })
+      .catch(() => {
+        alert('로그인이 필요합니다.');
+        navigate('/login');
+      });
   }, []);
 
   return (
