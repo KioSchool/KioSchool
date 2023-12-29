c39d7a94006da7bfe11b0e4e0c43f3c228df6aad
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-12-29 17:25:25 +0900

    Add global error handling for admin api and apply

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -5,6 +5,17 @@ const adminApi = axios.create({
   withCredentials: true,
 });
 
+adminApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 403) {
+      alert('로그인이 필요합니다.');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
+
 const userApi = axios.create({
   baseURL: process.env.REACT_APP_ENVIRONMENT == 'development' ? 'http://localhost:8080' : 'https://kio-school.fly.dev',
   withCredentials: true,
--- src/page/Admin/AdminHome.tsx
+++ src/page/Admin/AdminHome.tsx
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { adminApi } from '../../axios';
-import { useNavigate } from 'react-router-dom';
 
 function AdminHome() {
   const [url, setUrl] = useState();
-  const navigate = useNavigate();
 
   useEffect(() => {
-    adminApi
-      .get('/user')
-      .then((res) => {
-        setUrl(res.data.accountUrl);
-      })
-      .catch(() => {
-        alert('로그인이 필요합니다.');
-        navigate('/login');
-      });
+    adminApi.get('/user').then((res) => {
+      setUrl(res.data.accountUrl);
+    });
   }, []);
 
   return (
     <div>
       <div>AdminHome 여기가 헤더가 되어야 할듯?</div>
-      <div>{url}</div>
       <button onClick={() => window.open(`${url}&amount=5000`)}>test</button>
     </div>
   );
