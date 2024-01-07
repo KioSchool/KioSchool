5bf9d4e68010058080403416eb67ff832669de0b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-01-07 17:35:13 +0900

    Show accessible workspaces and add routes

--- src/page/Admin/AdminHome.tsx
+++ src/page/Admin/AdminHome.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useUser from '../../hook/useUser';
+import { useRecoilValue } from 'recoil';
+import { workspacesAtom } from '../../recoil/atoms';
+import useCustomNavigate from '../../hook/useCustomNavigate';
 
 function AdminHome() {
-  const { isLoggedIn } = useUser();
+  const { fetchWorkspaces } = useUser();
+  const { appendPath } = useCustomNavigate();
+  const workspaces = useRecoilValue(workspacesAtom);
+
+  useEffect(() => {
+    fetchWorkspaces();
+  }, []);
+
+  if (workspaces?.length === 0) {
+    return <div>워크스페이스가 없습니다.</div>;
+  }
 
   return (
     <div>
-      <div>AdminHome 여기가 헤더가 되어야 할듯?</div>
-      isLoggedIn: {isLoggedIn() ? 'true' : 'false'}
+      {workspaces.map((it) => (
+        <div key={it.id}>
+          {it.id} - {it.name}
+          <button
+            type={'button'}
+            onClick={() => {
+              appendPath(`/workspace/${it.id}`);
+            }}
+          >
+            이동
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
