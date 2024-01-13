e67fcdc4488c6e18f473d49e14ab9625bbdaaf81
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-01-14 01:41:25 +0900

    Add products atom

--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -1,5 +1,5 @@
 import { atom } from 'recoil';
-import { Order, Workspace } from '../type';
+import { Order, Product, Workspace } from '../type';
 
 export const ordersAtom = atom<Order[]>({
   key: 'ordersAtom',
@@ -8,5 +8,10 @@ export const ordersAtom = atom<Order[]>({
 
 export const workspacesAtom = atom<Workspace[]>({
   key: 'workspacesAtom',
+  default: [],
+});
+
+export const productsAtom = atom<Product[]>({
+  key: 'productsAtom',
   default: [],
 });
