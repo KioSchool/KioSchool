3fb7f57387e9cbd6c0cf2e61339b2a87b0e56ca5
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-01-20 00:58:16 +0900

    Add containers for center it

--- src/page/User/Login.tsx
+++ src/page/User/Login.tsx
@@ -4,11 +4,27 @@ import useAuthentication from '../../hook/useAuthentication';
 import AppInputWithLabel from '../../component/common/input/AppInputWithLabel';
 import AppButton from '../../component/common/button/AppButton';
 import styled from '@emotion/styled';
+import AppLabel from '../../component/common/label/AppLabel';
 
 const Container = styled.div`
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+`;
+
+const SubContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-basis: 0;
+  flex-wrap: wrap;
   align-items: center;
+  justify-content: center;
+  height: inherit;
+`;
+
+const LoginContainer = styled.div`
+  display: block;
+  position: relative;
 `;
 
 function Login() {
@@ -36,16 +52,20 @@ function Login() {
 
   return (
     <Container>
-      <h2>Login</h2>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <AppInputWithLabel label={'아이디'} type={'text'} id={'userId'} ref={userIdInputRef} required />
-          <AppInputWithLabel label={'비밀번호'} type={'password'} id={'password'} ref={userPasswordInputRef} required />
-        </div>
-        <AppButton type={'submit'}>로그인</AppButton>
-      </form>
-      <Link to={'/'}>Go Home</Link>
+      <SubContainer>
+        <LoginContainer>
+          <AppLabel>로그인</AppLabel>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <form onSubmit={handleSubmit}>
+            <div>
+              <AppInputWithLabel label={'아이디'} type={'text'} id={'userId'} ref={userIdInputRef} required />
+              <AppInputWithLabel label={'비밀번호'} type={'password'} id={'password'} ref={userPasswordInputRef} required />
+            </div>
+            <AppButton type={'submit'}>로그인</AppButton>
+          </form>
+          <Link to={'/'}>Go Home</Link>
+        </LoginContainer>
+      </SubContainer>
     </Container>
   );
 }
