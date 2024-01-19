981bd95c4af90076af5c9d8252fa0c3542fea5de
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-01-20 00:00:41 +0900

    implement createWorkspace function

--- src/page/Admin/AdminHome.tsx
+++ src/page/Admin/AdminHome.tsx
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import useUser from '../../hook/useUser';
 import { useRecoilValue } from 'recoil';
 import { workspacesAtom } from '../../recoil/atoms';
 import useCustomNavigate from '../../hook/useCustomNavigate';
 
 function AdminHome() {
-  const { fetchWorkspaces } = useUser();
+  const { fetchWorkspaces, createWorkspaces } = useUser();
   const { appendPath } = useCustomNavigate();
+  const userInputRef = useRef<HTMLInputElement>(null);
   const workspaces = useRecoilValue(workspacesAtom);
 
   useEffect(() => {
     fetchWorkspaces();
-  }, []);
+  }, [fetchWorkspaces]);
+
+  const onClickHandler = async () => {
+    const userInput = userInputRef.current?.value;
+    if (!userInput) {
+      alert('Please enter a workspace name.');
+      return;
+    }
+    createWorkspaces(userInput);
+  };
 
   if (workspaces?.length === 0) {
-    return <div>워크스페이스가 없습니다.</div>;
+    return (
+      <>
+        <div>워크스페이스가 없습니다.</div>
+        <input ref={userInputRef} type="text"></input>
+        <button onClick={onClickHandler}>생성하기</button>
+      </>
+    );
   }
 
   return (
