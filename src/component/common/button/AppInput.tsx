04d18659e528424df378e669d5a8f8a66b43c05a
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-01-19 23:46:38 +0900

    Add default style to AppInput

--- src/component/common/button/AppInput.tsx
+++ src/component/common/button/AppInput.tsx
@@ -1,9 +1,18 @@
 import React, { ForwardedRef, forwardRef } from 'react';
+import styled from '@emotion/styled';
 
 interface AppInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
+const Container = styled.input`
+  border: 1px solid black;
+  border-radius: 15px;
+  width: 500px;
+  height: 50px;
+  padding: 0 18px;
+`;
+
 const AppInput = forwardRef<HTMLInputElement, AppInputProps>((props: AppInputProps, ref: ForwardedRef<HTMLInputElement>) => {
-  return <input {...props} ref={ref} />;
+  return <Container {...props} ref={ref} />;
 });
 
 export default AppInput;
