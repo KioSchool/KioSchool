5f674ffbbe80b73d106d1860de4685bcac616c6d
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-01-20 20:26:07 +0900

    refatoring adminhome

--- src/page/Admin/AdminHome.tsx
+++ src/page/Admin/AdminHome.tsx
@@ -9,12 +9,14 @@ function AdminHome() {
   const { appendPath } = useCustomNavigate();
   const userInputRef = useRef<HTMLInputElement>(null);
   const workspaces = useRecoilValue(workspacesAtom);
+  let showWorkspaces = workspaces?.length !== 0;
 
   useEffect(() => {
     fetchWorkspaces();
-  }, [fetchWorkspaces]);
+  }, []);
 
-  const createHandler = () => {
+  const createHandler = (e: React.FormEvent) => {
+    e.preventDefault();
     const userInput = userInputRef.current?.value;
     if (!userInput) {
       alert('workspace 이름을 입력해주세요');
@@ -27,33 +29,32 @@ function AdminHome() {
     leaveWorkspaces(id);
   };
 
-  if (workspaces?.length === 0) {
-    return (
-      <>
-        <div>워크스페이스가 없습니다.</div>
-        <input ref={userInputRef} type="text"></input>
-        <button onClick={createHandler}>생성하기</button>
-      </>
-    );
-  }
-
   return (
-    <div>
-      {workspaces.map((it) => (
-        <div key={it.id}>
-          {it.id} - {it.name}
-          <button
-            type={'button'}
-            onClick={() => {
-              appendPath(`/workspace/${it.id}`);
-            }}
-          >
-            이동
-          </button>
-          <button onClick={() => leaveHandler(it.id)}>탈퇴하기</button>
+    <>
+      {showWorkspaces && (
+        <div>
+          {workspaces.map((it) => (
+            <div key={it.id}>
+              {it.id} - {it.name}
+              <button
+                type={'button'}
+                onClick={() => {
+                  appendPath(`/workspace/${it.id}`);
+                }}
+              >
+                이동
+              </button>
+              <button onClick={() => leaveHandler(it.id)}>탈퇴하기</button>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+
+      <form onSubmit={createHandler}>
+        <input ref={userInputRef} type="text"></input>
+        <button type="submit">생성하기</button>
+      </form>
+    </>
   );
 }
 
