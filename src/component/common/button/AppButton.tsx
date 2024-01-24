dec846d6eff2a50452461eacea669eeb229e3cc9
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-01-24 23:03:46 +0900

    Add AppButton custom size props

--- src/component/common/button/AppButton.tsx
+++ src/component/common/button/AppButton.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-interface AppButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
+export interface AppButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'small' | 'medium' | 'large';
+}
+const sizeMap = {
+  small: '120px',
+  medium: '160px',
+  large: '500px',
+};
 const Container = styled.button`
+  width: ${(props: AppButtonProps) => sizeMap[props.size || 'medium']};
   background: black;
   color: white;
   font-size: 18px;
   border: none;
   border-radius: 15px;
-  width: 500px;
   height: 50px;
   padding: 0 18px;
 `;
