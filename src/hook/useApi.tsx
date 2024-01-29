5ed6c7a42eec5e755739329e70085c90d7e39d24
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-01-29 21:56:03 +0900

    Add global loading modal interceptor to axsios

--- src/hook/useApi.tsx
+++ src/hook/useApi.tsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { isLoadingAtom } from '../recoil/atoms';
 
 function useApi() {
   const navigate = useNavigate();
+  const setIsLoading = useSetRecoilState(isLoadingAtom);
 
   const adminApi = axios.create({
     baseURL: process.env.REACT_APP_ENVIRONMENT == 'development' ? 'http://localhost:8080/admin' : 'https://kio-school.fly.dev/admin',
@@ -14,9 +17,24 @@ function useApi() {
     withCredentials: true,
   });
 
-  adminApi.interceptors.response.use(
-    (response) => response,
+  adminApi.interceptors.request.use(
+    (config) => {
+      setIsLoading(true);
+      return config;
+    },
     (error) => {
+      setIsLoading(false);
+      return Promise.reject(error);
+    },
+  );
+
+  adminApi.interceptors.response.use(
+    (response) => {
+      setIsLoading(false);
+      return response;
+    },
+    (error) => {
+      setIsLoading(false);
       if (error.response.status === 403) {
         alert('로그인이 필요합니다.');
         navigate('/login');
@@ -29,6 +47,28 @@ function useApi() {
     baseURL: process.env.REACT_APP_ENVIRONMENT == 'development' ? 'http://localhost:8080' : 'https://kio-school.fly.dev',
     withCredentials: true,
   });
+
+  userApi.interceptors.request.use(
+    (config) => {
+      setIsLoading(true);
+      return config;
+    },
+    (error) => {
+      setIsLoading(false);
+      return Promise.reject(error);
+    },
+  );
+
+  userApi.interceptors.response.use(
+    (response) => {
+      setIsLoading(false);
+      return response;
+    },
+    (error) => {
+      setIsLoading(false);
+      return Promise.reject(error);
+    },
+  );
 
   return { adminApi, userApi, sessionApi };
 }
