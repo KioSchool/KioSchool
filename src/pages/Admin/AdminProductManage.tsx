793c31f2c3e1551433ca444a9f43777dad0e9da4
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-02-06 02:23:01 +0900

    Refactor useState to useReducer

--- src/pages/Admin/AdminProductManage.tsx
+++ src/pages/Admin/AdminProductManage.tsx
@@ -1,36 +1,45 @@
+import React, { ChangeEvent, useState, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
 import AppButton from '@components/common/button/AppButton';
 import AppInputWithLabel from '@components/common/input/AppInputWithLabel';
 import SelectWithOptions from '@components/common/select/SelectWithOptions';
 import useAdminUser from '@hooks/useAdminUser';
-import { ChangeEvent, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case 'PRODUCT_NAME_INPUT':
+      return { ...state, name: action.payload };
+    case 'PRODUCT_DESCRIPTION_INPUT':
+      return { ...state, description: action.payload };
+    case 'PRODUCT_PRICE_INPUT':
+      return { ...state, price: action.payload };
+    case 'PRODUCT_WORKSPACEID_INPUT':
+      return { ...state, workspaceId: action.payload };
+    case 'PRODUCT_CATEGORY_INPUT':
+      return { ...state, productCategoryId: action.payload };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
 
 function AdminProductManage() {
-  const { addProduct } = useAdminUser();
   const { workspaceId } = useParams<{ workspaceId: string }>();
-  const productNameRef = useRef<HTMLInputElement>(null);
-  const productDescriptionRef = useRef<HTMLInputElement>(null);
-  const productPriceRef = useRef<HTMLInputElement>(null);
-  const productCategoryRef = useRef<HTMLSelectElement>(null);
+  const body = {
+    name: '',
+    description: '',
+    price: 0,
+    workspaceId: workspaceId,
+    productCategoryId: 'null',
+  };
+
+  const { addProduct } = useAdminUser();
   const [file, setFile] = useState<File | null>(null);
+  const [state, dispatch] = useReducer(reducer, body);
 
   const AddProduct = () => {
-    const name = productNameRef.current?.value;
-    const description = productDescriptionRef.current?.value;
-    const price = productPriceRef.current?.value;
-    const workspaceid = workspaceId;
-    const cat = productCategoryRef.current?.value;
-
-    const body = {
-      name: name,
-      description: description,
-      price: price,
-      workspaceId: workspaceid,
-      productCategoryId: cat,
-    };
-
+    dispatch({ type: 'PRODUCT_WORKSPACEID_INPUT', payload: workspaceId });
     if (file) {
-      addProduct(body, file);
+      addProduct(state, file);
     }
   };
 
@@ -46,17 +55,35 @@ function AdminProductManage() {
   return (
     <>
       <div>Product manage</div>
-      <AppInputWithLabel titleLabel={'상품 이름'} ref={productNameRef} />
-      <AppInputWithLabel titleLabel={'상품 설명'} ref={productDescriptionRef} />
-      <AppInputWithLabel type={'number'} titleLabel={'상품 가격'} ref={productPriceRef} />
+      <AppInputWithLabel
+        titleLabel={'상품 이름'}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          dispatch({ type: 'PRODUCT_NAME_INPUT', payload: e.target?.value });
+        }}
+      />
+      <AppInputWithLabel
+        titleLabel={'상품 설명'}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          dispatch({ type: 'PRODUCT_DESCRIPTION_INPUT', payload: e.target?.value });
+        }}
+      />
+      <AppInputWithLabel
+        type={'number'}
+        titleLabel={'상품 가격'}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          dispatch({ type: 'PRODUCT_PRICE_INPUT', payload: e.target?.value });
+        }}
+      />
       <input type="file" id="img" accept="image/*" onChange={onImageChange} />
       <SelectWithOptions
         options={[
           { name: '기본', val: 'null' },
           { name: '인기', val: 1 },
         ]}
-        ref={productCategoryRef}
-      ></SelectWithOptions>
+        onInput={(event: React.ChangeEvent<HTMLSelectElement>) => {
+          dispatch({ type: 'PRODUCT_CATEGORY_INPUT', payload: event.target.value });
+        }}
+      />
       <AppButton onClick={AddProduct}>추가하기</AppButton>
     </>
   );
