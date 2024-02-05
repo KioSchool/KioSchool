ef6c23c7d5b1d44d10814ca68bcbc2f15d839d1a
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-02-05 14:59:47 +0900

    Add basic layout to add new product.
    using ref to get user input.

--- src/pages/Admin/AdminProductManage.tsx
+++ src/pages/Admin/AdminProductManage.tsx
@@ -1,5 +1,21 @@
+import AppButton from '@components/common/button/AppButton';
+import AppInputWithLabel from '@components/common/input/AppInputWithLabel';
+import { useRef } from 'react';
+
 function AdminProductManage() {
-  return <div>Product manage</div>;
+  const productNameRef = useRef<HTMLInputElement>(null);
+  const productDescriptionRef = useRef<HTMLInputElement>(null);
+  const productPriceRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <div>Product manage</div>
+      <AppInputWithLabel titleLabel={'상품 이름'} ref={productNameRef} />
+      <AppInputWithLabel titleLabel={'상품 설명'} ref={productDescriptionRef} />
+      <AppInputWithLabel titleLabel={'상품 가격'} ref={productPriceRef} />
+      <AppButton>추가하기</AppButton>
+    </>
+  );
 }
 
 export default AdminProductManage;
