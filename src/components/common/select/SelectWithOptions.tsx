ec4bb71fbc350a211f9489c9055168107ad0fd2b
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-02-06 17:12:31 +0900

    Add default option in SelectWithOption

--- src/components/common/select/SelectWithOptions.tsx
+++ src/components/common/select/SelectWithOptions.tsx
@@ -2,17 +2,21 @@ import { SelectHTMLAttributes, forwardRef } from 'react';
 
 interface Option {
   name: string;
-  id: number;
+  id: number | string;
 }
 
 interface SelectWithOptionsProps extends SelectHTMLAttributes<HTMLSelectElement> {
   options: Option[];
 }
+
 const SelectWithOptions = forwardRef<HTMLSelectElement, SelectWithOptionsProps>(({ options, value, ...rest }, ref) => {
+  const defaultOption = { name: '기본', id: 'null' };
+  const allOptions = [defaultOption, ...options];
+
   return (
     <div>
       <select ref={ref} value={value} {...rest}>
-        {options.map((item) => (
+        {allOptions.map((item) => (
           <option key={item.id} value={item.id}>
             {item.name}
           </option>
