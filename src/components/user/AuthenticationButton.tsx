f2bb9cf0c2c8f4721fb8ad05180fc7f9ccac440b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-02-06 22:32:58 +0900

    Remove useless fragment

--- src/components/user/AuthenticationButton.tsx
+++ src/components/user/AuthenticationButton.tsx
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import useAuthentication from '@hooks/useAuthentication';
 
 function AuthenticationButton() {
   const { logout, isLoggedIn } = useAuthentication();
 
-  return <Fragment>{isLoggedIn() ? <p onClick={logout}>Logout</p> : <Link to={'/login'}>Login</Link>}</Fragment>;
+  return <>{isLoggedIn() ? <p onClick={logout}>Logout</p> : <Link to={'/login'}>Login</Link>}</>;
 }
 
 export default AuthenticationButton;
