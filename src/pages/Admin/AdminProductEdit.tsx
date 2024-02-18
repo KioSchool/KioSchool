ba10b17d47e22c3d8f192da1594a25f664cf2f6f
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-02-18 23:33:52 +0900

    Bring code from AdminProductManage

--- src/pages/Admin/AdminProductEdit.tsx
+++ src/pages/Admin/AdminProductEdit.tsx
@@ -1,24 +1,136 @@
+import { Product, ProductActionType } from '@@types/index';
+import AppButton from '@components/common/button/AppButton';
+import AppInputWithLabel from '@components/common/input/AppInputWithLabel';
+import SelectWithOptions from '@components/common/select/SelectWithOptions';
+import styled from '@emotion/styled';
 import UseProducts from '@hooks/useProducts';
-import { useEffect } from 'react';
+import { userWorkspaceAtom } from '@recoils/atoms';
+import { ChangeEvent, useEffect, useReducer, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
+const ErrorMessage = styled.div`
+  padding: 0 0 5px;
+  color: #ff0000;
+`;
+
+const initState: Product = {
+  name: '',
+  description: '',
+  price: 0,
+  imageUrl: '',
+  id: 0,
+  createdAt: '',
+  updatedAt: '',
+  productCategory: {
+    id: 0,
+    name: '',
+    createdAt: '',
+    updatedAt: '',
+  },
+};
+
+function reducer(state: Product, action: ProductActionType) {
+  switch (action.type) {
+    case 'PRODUCT_NAME_INPUT':
+      return { ...state, name: action.payload };
+    case 'PRODUCT_DESCRIPTION_INPUT':
+      return { ...state, description: action.payload };
+    case 'PRODUCT_PRICE_INPUT':
+      return { ...state, price: action.payload };
+    case 'PRODUCT_CATEGORY_INPUT':
+      return { ...state, productCategoryId: action.payload };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
 
 function AdminProductEdit() {
   const { fetchProduct } = UseProducts(undefined);
+  const workspace = useRecoilValue(userWorkspaceAtom);
+
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [file, setFile] = useState<File | null>(null);
 
   const [searchParams] = useSearchParams();
   const productId = Number(searchParams.get('productId'));
 
+  const [product, setProduct] = useState<Product>();
+
+  const [productState, dispatch] = useReducer(reducer, initState);
+
   useEffect(() => {
     (async () => {
       const ret = await fetchProduct(productId);
-      console.log(ret);
+      setProduct(ret);
+
+      dispatch({ type: 'PRODUCT_NAME_INPUT', payload: ret.name });
+      dispatch({ type: 'PRODUCT_DESCRIPTION_INPUT', payload: ret.description });
+      dispatch({ type: 'PRODUCT_PRICE_INPUT', payload: ret.price });
     })();
   }, []);
 
+  const EditProduct = () => {
+    if (!productState.name || !productState.description) {
+      setErrorMessage('상품 이름 및 설명을 입력해주세요');
+      return;
+    }
+    if (productState.price < 0) {
+      setErrorMessage('가격은 음수가 될 수 없습니다.');
+      return;
+    }
+    if (!file) {
+      setErrorMessage('파일을 선택해주세요');
+      return;
+    }
+    setErrorMessage('');
+    console.log(product);
+    // addProduct(state, file);
+  };
+
+  const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.length) {
+      setFile(null);
+      return;
+    }
+
+    setFile(event.target.files[0]);
+  };
   return (
     <>
       <h1>Edit Page</h1>
       {productId}
+      {errorMessage && <ErrorMessage className="error-message">{errorMessage}</ErrorMessage>}
+      <AppInputWithLabel
+        titleLabel={'상품 이름'}
+        value={product?.name}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          dispatch({ type: 'PRODUCT_NAME_INPUT', payload: event.target?.value });
+        }}
+      />
+      <AppInputWithLabel
+        titleLabel={'상품 설명'}
+        value={product?.description}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          dispatch({ type: 'PRODUCT_DESCRIPTION_INPUT', payload: event.target?.value });
+        }}
+      />
+      <AppInputWithLabel
+        type={'number'}
+        titleLabel={'상품 가격'}
+        value={product?.price}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          dispatch({ type: 'PRODUCT_PRICE_INPUT', payload: event.target?.value });
+        }}
+      />
+      <input type="file" id="img" accept="image/*" onChange={onImageChange} />
+      <SelectWithOptions
+        options={workspace.productCategories}
+        onInput={(event: React.ChangeEvent<HTMLSelectElement>) => {
+          dispatch({ type: 'PRODUCT_CATEGORY_INPUT', payload: event.target.value });
+        }}
+      />
+      <AppButton onClick={EditProduct}>변경하기</AppButton>
     </>
   );
 }
