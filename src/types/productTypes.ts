2b3155a82b9b5094330eca3e6fbf6b65482b3cc8
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-02 21:41:30 +0900

    Move ProductEdit and rename

--- src/types/productTypes.ts
+++ src/types/productTypes.ts
@@ -1,5 +1,23 @@
 import { Product } from '.';
 
-export interface ProductEdit extends Product {
+export interface ProductAddedImage extends Product {
   image: {};
 }
+export type ProductEdit = Omit<ProductAddedImage, 'createdAt' | 'updatedAt' | 'imageUrl'>;
+
+export const initState: ProductEdit = {
+  name: '',
+  description: '',
+  price: 0,
+  id: 0,
+  image: {
+    url: '',
+    file: null,
+  },
+  productCategory: {
+    id: 0,
+    name: '',
+    createdAt: '',
+    updatedAt: '',
+  },
+};
