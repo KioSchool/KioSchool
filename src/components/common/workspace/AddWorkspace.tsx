f7fbf927f4b35782daee8674f4c4fd244af05278
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-23 21:35:15 +0900

    변수 상수화 및 조건부 렌더링을 early return 으로 변경

--- src/components/common/workspace/AddWorkspace.tsx
+++ src/components/common/workspace/AddWorkspace.tsx
@@ -1,14 +1,14 @@
 import { AddWorkspaceContainer } from './Container';
 import plusLogo from '../../../resources/image/plusLogo.png';
 const AddWorkspace = (props: any) => {
+  const maxWorkspaceNum = 3;
+
+  if (props.workspaces.length >= maxWorkspaceNum) return null;
+
   return (
-    <>
-      {props.workspaces.length < 3 && (
-        <AddWorkspaceContainer onClick={props.modalOpen}>
-          <img src={plusLogo} width={'51px'} height={'51px'}></img>
-        </AddWorkspaceContainer>
-      )}
-    </>
+    <AddWorkspaceContainer onClick={props.modalOpen}>
+      <img src={plusLogo} width={'51px'} height={'51px'}></img>
+    </AddWorkspaceContainer>
   );
 };
 
