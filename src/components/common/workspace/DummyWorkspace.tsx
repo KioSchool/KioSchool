fdb358dc64fd97e8e3a514649a8ae0558ac6e2ce
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-23 21:38:18 +0900

    변수 상수화

--- src/components/common/workspace/DummyWorkspace.tsx
+++ src/components/common/workspace/DummyWorkspace.tsx
@@ -1,10 +1,12 @@
 import { DummyWorkspaceContainer } from './Container';
 
 const DummyWorkspace = (props: any) => {
+  const maxDummyWorkspaceNum = 2;
+
   return (
     <>
-      {props.workspaces.length < 2 &&
-        Array.from({ length: 2 - props.workspaces.length }, (_, index) => <DummyWorkspaceContainer key={`dummy_${index}`}></DummyWorkspaceContainer>)}
+      {props.workspaces.length < maxDummyWorkspaceNum &&
+        Array.from({ length: maxDummyWorkspaceNum - props.workspaces.length }, (_, index) => <DummyWorkspaceContainer key={`dummy_${index}`} />)}
     </>
   );
 };
