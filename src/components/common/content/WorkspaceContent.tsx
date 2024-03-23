4113c772e11dd2baa21d65ed73240d523dd8afd6
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-23 22:14:45 +0900

    워크스페이스 내용 별도의 컴포넌트로 분리

---
+++ src/components/common/content/WorkspaceContent.tsx
@@ -0,0 +1,131 @@
+import { Workspace } from '@@types/index';
+import styled from '@emotion/styled';
+import useCustomNavigate from '@hooks/useCustomNavigate';
+
+const WorkspaceContainer = styled.div`
+  cursor: pointer;
+  width: 321px;
+  height: 332px;
+  border-radius: 25px;
+  background: #eb6d09;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  &:hover {
+    background: linear-gradient(140deg, #ffe3cc -165.17%, #eb6d09 92.63%);
+  }
+
+  &:active {
+    border-radius: 25px;
+    background: #eb6d09;
+    box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.25) inset;
+  }
+`;
+
+const MenuTitle = styled.div`
+  padding: 0 0 25px 22px;
+  width: 164px;
+  height: 27px;
+  flex-shrink: 0;
+  color: #fff;
+  font-size: 24px;
+  font-weight: 300;
+  display: flex;
+  flex-direction: row;
+`;
+
+const DeleteContainer = styled.div`
+  padding: 16px 23px 0 0;
+`;
+
+const DeleteText = styled.div`
+  cursor: pointer;
+  color: #fff;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  flex-direction: column;
+`;
+
+const MainTitleContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SubTitleContainer = styled.div`
+  display: block;
+`;
+
+const Description = styled.div`
+  padding: 26px 0 0 22px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 198px;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 100;
+`;
+
+const Title = styled.div`
+  padding: 5px 0 0 22px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 188px;
+  height: 45px;
+  color: #fff;
+  font-size: 40.329px;
+  font-weight: 700;
+`;
+
+type WrapperProps = {
+  workspaces: Workspace[];
+  leaveHandler: (e: React.FormEvent, id: number) => void;
+};
+
+const WorkspaceContent = ({ workspaces, leaveHandler }: WrapperProps) => {
+  const { appendPath } = useCustomNavigate();
+  return (
+    <>
+      {workspaces.map((it) => (
+        <WorkspaceContainer
+          key={it.id}
+          onClick={() => {
+            appendPath(`/workspace/${it.id}`);
+          }}
+        >
+          <TitleContainer>
+            <MainTitleContainer>
+              <Description>{it.description}</Description>
+              <DeleteContainer>
+                <DeleteText
+                  onClick={(e: React.FormEvent) => {
+                    leaveHandler(e, it.id);
+                  }}
+                >
+                  탈퇴하기
+                </DeleteText>
+              </DeleteContainer>
+            </MainTitleContainer>
+
+            <SubTitleContainer>
+              <Title>{it.name}</Title>
+            </SubTitleContainer>
+          </TitleContainer>
+
+          <MenuTitle>메뉴 개수 {it.products.length} 개</MenuTitle>
+        </WorkspaceContainer>
+      ))}
+    </>
+  );
+};
+
+export default WorkspaceContent;
--- src/pages/Admin/AdminHome.tsx
+++ src/pages/Admin/AdminHome.tsx
@@ -4,7 +4,6 @@ import { useRecoilValue } from 'recoil';
 import { workspacesAtom } from '@recoils/atoms';
 import userDefaultProfileImage from '@resources/image/userDefaultProfileImage.png';
 import kioLogo from '@resources/image/kioLogo.png';
-import useCustomNavigate from '@hooks/useCustomNavigate';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import InputModal from '@components/common/modal/InputModal';
@@ -12,29 +11,7 @@ import Container from '@components/common/container/Container';
 import DummyWorkspace from '@components/common/workspace/DummyWorkspace';
 import AddWorkspace from '@components/common/workspace/AddWorkspace';
 import { AddWorkspaceModalContent } from '@components/common/content/AddworkspaceModalContent';
-
-const WorkspaceContainer = styled.div`
-  cursor: pointer;
-  width: 321px;
-  height: 332px;
-  border-radius: 25px;
-  background: #eb6d09;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-
-  &:hover {
-    background: linear-gradient(140deg, #ffe3cc -165.17%, #eb6d09 92.63%);
-  }
-
-  &:active {
-    border-radius: 25px;
-    background: #eb6d09;
-    box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.25) inset;
-  }
-`;
+import WorkspaceContent from '@components/common/content/WorkspaceContent';
 
 const NavContainer = styled.div`
   padding-bottom: 40px;
@@ -50,70 +27,9 @@ const LinkItem = styled(Link)`
   height: 60px;
 `;
 
-const MenuTitle = styled.div`
-  padding: 0 0 25px 22px;
-  width: 164px;
-  height: 27px;
-  flex-shrink: 0;
-  color: #fff;
-  font-size: 24px;
-  font-weight: 300;
-  display: flex;
-  flex-direction: row;
-`;
-
-const DeleteContainer = styled.div`
-  padding: 16px 23px 0 0;
-`;
-
-const DeleteText = styled.div`
-  cursor: pointer;
-  color: #fff;
-`;
-
-const TitleContainer = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-  flex-direction: column;
-`;
-
-const MainTitleContainer = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const SubTitleContainer = styled.div`
-  display: block;
-`;
-
-const Description = styled.div`
-  padding: 26px 0 0 22px;
-  display: flex;
-  flex-wrap: wrap;
-  width: 198px;
-  color: #fff;
-  font-size: 32px;
-  font-weight: 100;
-`;
-
-const Title = styled.div`
-  padding: 5px 0 0 22px;
-  display: flex;
-  flex-wrap: wrap;
-  width: 188px;
-  height: 45px;
-  color: #fff;
-  font-size: 40.329px;
-  font-weight: 700;
-`;
-
 function AdminHome() {
   const { fetchWorkspaces, createWorkspaces, leaveWorkspaces } = useAdminUser();
-  const { appendPath } = useCustomNavigate();
+
   const workspaceNameRef = useRef<HTMLInputElement>(null);
   const workspaceDescriptionRef = useRef<HTMLInputElement>(null);
   const workspaces = useRecoilValue(workspacesAtom);
@@ -170,35 +86,7 @@ function AdminHome() {
             </LinkItem>
           </NavContainer>
 
-          {workspaces.map((it) => (
-            <WorkspaceContainer
-              key={it.id}
-              onClick={() => {
-                appendPath(`/workspace/${it.id}`);
-              }}
-            >
-              <TitleContainer>
-                <MainTitleContainer>
-                  <Description>{it.description}</Description>
-                  <DeleteContainer>
-                    <DeleteText
-                      onClick={(e: React.FormEvent) => {
-                        leaveHandler(e, it.id);
-                      }}
-                    >
-                      탈퇴하기
-                    </DeleteText>
-                  </DeleteContainer>
-                </MainTitleContainer>
-
-                <SubTitleContainer>
-                  <Title>{it.name}</Title>
-                </SubTitleContainer>
-              </TitleContainer>
-
-              <MenuTitle>메뉴 개수 {it.products.length} 개</MenuTitle>
-            </WorkspaceContainer>
-          ))}
+          <WorkspaceContent workspaces={workspaces} leaveHandler={leaveHandler} />
 
           <AddWorkspace workspaces={workspaces} modalOpen={openModal} />
           <DummyWorkspace workspaces={workspaces} />
