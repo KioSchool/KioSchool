837524af19063a33207cf94d2c3b995687e944e4
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-27 01:09:42 +0900

    내 주점 바로가기 버튼 디자인 적

--- src/pages/User/Home.tsx
+++ src/pages/User/Home.tsx
@@ -3,12 +3,41 @@ import { Link } from 'react-router-dom';
 import AuthenticationButton from '@components/user/AuthenticationButton';
 import Container from '@components/common/container/Container';
 import styled from '@emotion/styled';
+import ArrowRight from '../../resources/svg/ArrowRight';
 
 const MainTitle = styled.div`
   width: 540px;
   height: 160px;
   font-size: 64px;
   font-weight: 800;
+`;
+
+const LinkAdminHome = styled(Link)`
+  text-decoration: none;
+  width: 348.695px;
+  height: 80px;
+  border-radius: 50px;
+  background: #eb6d09;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background: linear-gradient(133deg, #ff9f32, #eb6d09);
+  }
+`;
+
+const LinkText = styled.div`
+  width: 314.256px;
+  height: 47px;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
 
 function Home() {
@@ -20,7 +49,12 @@ function Home() {
             키오스쿨로
             <br /> 주점관리를 손쉽게,
           </MainTitle>
-          <Link to={'/admin'}>Admin</Link>
+          <LinkAdminHome to={'/admin'}>
+            <LinkText>
+              내 주점 바로가기
+              <ArrowRight />
+            </LinkText>
+          </LinkAdminHome>
           <br></br>
           <AuthenticationButton />
         </>
