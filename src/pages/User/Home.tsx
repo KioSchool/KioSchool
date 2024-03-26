9be1dc6f311539fadfcecaa76765e687044dd18a
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-27 00:48:28 +0900

    홈페이지 메인 타이틀 작성

--- src/pages/User/Home.tsx
+++ src/pages/User/Home.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthenticationButton from '@components/user/AuthenticationButton';
+import Container from '@components/common/container/Container';
+import styled from '@emotion/styled';
+
+const MainTitle = styled.div`
+  width: 540px;
+  height: 160px;
+  font-size: 64px;
+  font-weight: 800;
+`;
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <Link to={'/admin'}>Admin</Link>
-      <br></br>
-      <AuthenticationButton />
+      <Container justifyValue={'center'}>
+        <>
+          <MainTitle>
+            키오스쿨로
+            <br /> 주점관리를 손쉽게,
+          </MainTitle>
+          <Link to={'/admin'}>Admin</Link>
+          <br></br>
+          <AuthenticationButton />
+        </>
+      </Container>
     </>
   );
 }
