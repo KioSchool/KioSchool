a8e2cd33af6ffd0bed492df6b8635525bcc7dc0b
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-03-27 01:45:01 +0900

    svg 속성들 카멜케이스로 변경

--- src/resources/svg/ArrowRight.tsx
+++ src/resources/svg/ArrowRight.tsx
@@ -4,8 +4,8 @@ interface ArrowRightSvgProps extends React.SVGProps<SVGSVGElement> {}
 
 const ArrowRight = (props: ArrowRightSvgProps) => (
   <svg width={34} height={34} viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path id="Vector" d="M5.3125 17H28.6875" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-    <path id="Vector_2" d="M19.125 7.4375L28.6875 17L19.125 26.5625" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+    <path id="Vector" d="M5.3125 17H28.6875" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+    <path id="Vector_2" d="M19.125 7.4375L28.6875 17L19.125 26.5625" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 export default ArrowRight;
