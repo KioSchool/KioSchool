854ae52f9e620019b68b68477985976692063e13
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-04-02 02:25:16 +0900

    Create TitleNavBar.tsx

---
+++ src/components/common/nav/TitleNavBar.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import ArrowLeftSvg from '@resources/svg/ArrowLeftSvg';
+import AppLabel from '@components/common/label/AppLabel';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+  title: string;
+  useBackIcon?: boolean;
+  rightElement?: JSX.Element;
+}
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 200px;
+  justify-content: center;
+  height: 50px;
+  width: 100%;
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1200px;
+`;
+
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 25px;
+`;
+
+function TitleNavBar({ title, useBackIcon = true, rightElement }: Props) {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <SubContainer>
+        <LeftContainer>
+          <ArrowLeftSvg
+            style={{ display: useBackIcon ? '' : 'hidden', cursor: 'pointer' }}
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+          <AppLabel size={36} style={{ fontWeight: 800 }}>
+            {title}
+          </AppLabel>
+        </LeftContainer>
+        {rightElement}
+      </SubContainer>
+    </Container>
+  );
+}
+
+export default TitleNavBar;
