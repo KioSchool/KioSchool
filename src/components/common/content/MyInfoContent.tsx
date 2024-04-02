dc0ad275ab9f66be6bd21f416b133b2e975725ce
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-04-02 20:21:45 +0900

    MyInfo서브컨테이너 width 조절

--- src/components/common/content/MyInfoContent.tsx
+++ src/components/common/content/MyInfoContent.tsx
@@ -14,12 +14,11 @@ const MyInfoContainer = styled.div`
   box-shadow: 0px 4px 16.8px 0px rgba(0, 0, 0, 0.25);
 `;
 const MyInfoSubContainer = styled.div`
-  border: 2px solid black;
-  width: 880px;
+  width: 100%;
   height: 220px;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
 `;
 
