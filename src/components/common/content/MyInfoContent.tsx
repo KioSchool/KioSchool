4da26d0c542b65de5189804781515036eed1ddad
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-04-02 19:37:14 +0900

    세로 줄 추가

--- src/components/common/content/MyInfoContent.tsx
+++ src/components/common/content/MyInfoContent.tsx
@@ -16,11 +16,17 @@ const MyInfoContainer = styled.div`
 const MyInfoSubContainer = styled.div`
   border: 2px solid black;
   width: 880px;
-  height: 215px;
+  height: 220px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+`;
+
+const VerticalLine = styled.div`
+  width: 1px;
+  height: 215px;
+  background: #ccc;
 `;
 
 function MyInfoContent() {
@@ -29,7 +35,9 @@ function MyInfoContent() {
       <MyInfoContainer>
         <MyInfoSubContainer>
           <MyInfoItemContent label="비밀번호 변경" />
+          <VerticalLine />
           <MyInfoItemContent label="계좌관리" />
+          <VerticalLine />
           <MyInfoItemContent label="계정탈퇴" />
         </MyInfoSubContainer>
       </MyInfoContainer>
