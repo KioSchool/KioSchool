1b851dffb660e199252b1c8506295c2f6bc8d7e8
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-04-02 20:48:40 +0900

    titleNavBar에 사용자 이름 추가

--- src/pages/User/MyInfo.tsx
+++ src/pages/User/MyInfo.tsx
@@ -2,6 +2,10 @@ import Container from '@components/common/container/Container';
 import MyInfoContent from '@components/common/content/MyInfoContent';
 import TitleNavBar from '@components/common/nav/TitleNavBar';
 import styled from '@emotion/styled';
+import useAdminUser from '@hooks/useAdminUser';
+import { adminUserAtom } from '@recoils/atoms';
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 
 const MyInfoContainer = styled.div`
   display: flex;
@@ -11,11 +15,16 @@ const MyInfoContainer = styled.div`
 `;
 
 function MyInfo() {
+  const { fetchAdminUser } = useAdminUser();
+  const user = useRecoilValue(adminUserAtom);
+  useEffect(() => {
+    fetchAdminUser();
+  }, []);
   return (
     <>
       <Container justifyValue={'center'} alignItems={'center'} flexDirection={'column'}>
         <MyInfoContainer>
-          <TitleNavBar title={'님의 마이페이지'} useBackIcon={true} />
+          <TitleNavBar title={`${user.name} 님의 마이페이지`} useBackIcon={true} />
           <MyInfoContent />
         </MyInfoContainer>
       </Container>
