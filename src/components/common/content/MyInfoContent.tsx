8d6476ea374f6b3a748c98aee9458a339febb0e3
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-04-02 19:30:04 +0900

    마이페이지 contents 컨테이너 생성 및 아이템 배치

--- src/components/common/content/MyInfoContent.tsx
+++ src/components/common/content/MyInfoContent.tsx
@@ -1,22 +1,90 @@
 import styled from '@emotion/styled';
+import DeleteButtonSvg from '@resources/svg/DeleteButtonSvg';
 
 const MyInfoContainer = styled.div`
   width: 1100px;
   height: 300px;
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   gap: 10px;
   flex-shrink: 0;
   border-radius: 20px;
-  background: #fff;
   box-shadow: 0px 4px 16.8px 0px rgba(0, 0, 0, 0.25);
 `;
+const MyInfoSubContainer = styled.div`
+  border: 2px solid black;
+  width: 880px;
+  height: 215px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+const ItemContainer = styled.div`
+  background: silver;
+  border: 2px solid black;
+  width: 160px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.div`
+  position: relative;
+  width: 156px;
+  height: 70px;
+`;
+
+const DeleteButton = styled(DeleteButtonSvg)`
+  position: absolute;
+  width: 70px;
+  height: 70px;
+  right: 44px;
+  left: 44px;
+  top: 12px;
+  transition: transform 0.1s ease;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+const ItmeLabel = styled.div`
+  width: 156px;
+  height: 43px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+`;
 function MyInfoContent() {
   return (
     <>
-      <MyInfoContainer></MyInfoContainer>
+      <MyInfoContainer>
+        <MyInfoSubContainer>
+          <ItemContainer>
+            <ButtonContainer>
+              <DeleteButton />
+            </ButtonContainer>
+            <ItmeLabel>비밀번호 변경</ItmeLabel>
+          </ItemContainer>
+          <ItemContainer>
+            <ButtonContainer>
+              <DeleteButton />
+            </ButtonContainer>
+            <ItmeLabel>계좌 관리</ItmeLabel>
+          </ItemContainer>
+          <ItemContainer>
+            <ButtonContainer>
+              <DeleteButton />
+            </ButtonContainer>
+            <ItmeLabel>계정 탈퇴</ItmeLabel>
+          </ItemContainer>
+        </MyInfoSubContainer>
+      </MyInfoContainer>
     </>
   );
 }
