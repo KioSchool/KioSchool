87b812b222af27ccefc769f05cd27469cfc4dc6c
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-04-30 02:12:31 +0900

    카테고리 컨텐츠 및 버튼 컨테이너 생성

--- src/pages/Admin/AdminProductCategoires.tsx
+++ src/pages/Admin/AdminProductCategoires.tsx
@@ -1,11 +1,46 @@
+import AppContainer from '@components/common/container/AppContainer';
 import NavBar from '@components/common/nav/NavBar';
 import TitleNavBar from '@components/common/nav/TitleNavBar';
+import styled from '@emotion/styled';
+import AppButton from '@components/common/button/AppButton';
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  align-items: center;
+  padding-top: 100px;
+  padding-bottom: 100px;
+`;
+const CategoriesContentContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const CategoriesButtonContainer = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: skyBlue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 function AdminProductCategories() {
   return (
     <>
       <NavBar useBackground={true} />
-      <TitleNavBar title={'카테고리 관리'}></TitleNavBar>
+      <AppContainer justifyValue={'center'}>
+        <Container>
+          <TitleNavBar title={'카테고리 관리'}></TitleNavBar>
+          <CategoriesContentContainer />
+          <CategoriesButtonContainer>
+            <AppButton size={'medium'}>편집 완료</AppButton>
+          </CategoriesButtonContainer>
+        </Container>
+      </AppContainer>
     </>
   );
 }
