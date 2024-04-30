8fee9e082a3299620ad2c5ecbab3d4e06dae880c
Rune <sungjong5k@naver.com>
2024-04-30 16:42:05 +0900

    react-beautiful-dnd를 이용한 드로그앤드랍 시범 적용

--- src/pages/Admin/AdminProductCategoires.tsx
+++ src/pages/Admin/AdminProductCategoires.tsx
@@ -4,6 +4,8 @@ import TitleNavBar from '@components/common/nav/TitleNavBar';
 import styled from '@emotion/styled';
 import AppButton from '@components/common/button/AppButton';
 import AppInputWithButton from '@components/common/input/AppInputWithButton';
+import { useEffect, useState } from 'react';
+import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -20,6 +22,7 @@ const CategoriesInputContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 
 const CategoriesContentContainer = styled.div`
@@ -29,6 +32,7 @@ const CategoriesContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  overflow: auto;
 `;
 
 const CategoriesButtonContainer = styled.div`
@@ -40,7 +44,31 @@ const CategoriesButtonContainer = styled.div`
   align-items: center;
 `;
 
+const CategoriesContens = styled.div`
+  width: 500px;
+  height: 60px;
+  background-color: white;
+  border-radius: 12px;
+  margin: 10px 0;
+`;
+
 function AdminProductCategories() {
+  const items = [...Array(10)].map((_, i) => ({ id: `${i}${i}${i}`, content: `item-${i}` }));
+
+  const [enabled, setEnabled] = useState(false);
+  const onDragEnd = ({}: DropResult) => {};
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
   return (
     <>
       <NavBar useBackground={true} />
@@ -50,7 +78,26 @@ function AdminProductCategories() {
           <CategoriesInputContainer>
             <AppInputWithButton />
           </CategoriesInputContainer>
-          <CategoriesContentContainer />
+          <CategoriesContentContainer>
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {items.map((item, index) => (
+                      <Draggable key={item.id} draggableId={item.id} index={index}>
+                        {(pro) => (
+                          <CategoriesContens ref={pro.innerRef} {...pro.draggableProps} {...pro.dragHandleProps}>
+                            {item.content}
+                          </CategoriesContens>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </CategoriesContentContainer>
           <CategoriesButtonContainer>
             <AppButton size={'medium'}>편집 완료</AppButton>
           </CategoriesButtonContainer>
