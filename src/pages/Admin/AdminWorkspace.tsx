36da6b23b79e7b774e3980677e8b50cdf00c3ec3
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-05-03 19:44:00 +0900

    Edit apply image route button to admin workspace but image is dummy

--- src/pages/Admin/AdminWorkspace.tsx
+++ src/pages/Admin/AdminWorkspace.tsx
@@ -2,10 +2,29 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useCustomNavigate from '@hooks/useCustomNavigate';
 import useAdminWorkspace from '@hooks/admin/useAdminWorkspace';
+import TitleNavBar from '@components/common/nav/TitleNavBar';
+import { useRecoilValue } from 'recoil';
+import { userWorkspaceAtom } from '@recoils/atoms';
+import styled from '@emotion/styled';
+import ImageRouteButton from '@components/common/button/ImageRouteButton';
+import AppContainer from '@components/common/container/AppContainer';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 30px;
+`;
 
 function AdminWorkspace() {
   const { workspaceId } = useParams<{ workspaceId: string }>();
   const { fetchWorkspace } = useAdminWorkspace();
+  const workspace = useRecoilValue(userWorkspaceAtom);
   const { appendPath } = useCustomNavigate();
 
   useEffect(() => {
@@ -13,18 +32,28 @@ function AdminWorkspace() {
   }, []);
 
   return (
-    <div>
-      <div>workspaceId: {workspaceId}</div>
-      <button type={'button'} onClick={() => appendPath('/orders')}>
-        실시간 주문 조회
-      </button>
-      <button type={'button'} onClick={() => appendPath('/orders-history')}>
-        전체 주문 조회
-      </button>
-      <button type={'button'} onClick={() => appendPath('/products')}>
-        상품 조회
-      </button>
-    </div>
+    <AppContainer justifyValue={'center'}>
+      <Container>
+        <TitleNavBar title={workspace.name} subTitle={workspace.description} />
+        <ButtonContainer>
+          <ImageRouteButton
+            src={'https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/1217/IE003091261_STD.jpg'}
+            onClick={() => appendPath('/orders')}
+            buttonText={'실시간 주문 조회'}
+          />
+          <ImageRouteButton
+            src={'https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/1217/IE003091261_STD.jpg'}
+            onClick={() => appendPath('/orders-history')}
+            buttonText={'전체 주문 조회'}
+          />
+          <ImageRouteButton
+            src={'https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/1217/IE003091261_STD.jpg'}
+            onClick={() => appendPath('/products')}
+            buttonText={'상품 조회'}
+          />
+        </ButtonContainer>
+      </Container>
+    </AppContainer>
   );
 }
 
