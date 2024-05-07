e1d63b7160ef9c3944362974c249fa0edea24a92
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-05-07 11:49:49 +0900

    Edit admin order page layout

--- src/pages/Admin/order/AdminOrder.tsx
+++ src/pages/Admin/order/AdminOrder.tsx
@@ -8,14 +8,67 @@ import NotPaidOrderCard from '@components/admin/order/NotPaidOrderCard';
 import ServedOrderCard from '@components/admin/order/ServedOrderCard';
 import useAdminOrder from '@hooks/admin/useAdminOrder';
 import styled from '@emotion/styled';
+import TitleNavBar from '@components/common/nav/TitleNavBar';
+import VerticalDivider from '@components/common/divider/VerticalDivider';
+import AppLabel from '@components/common/label/AppLabel';
 
 const Container = styled.div`
-  padding: 24px;
   display: flex;
   flex-direction: column;
-  flex-wrap: wrap;
   gap: 24px;
+  width: 100vw;
+  height: 100vh;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const KanbanContainer = styled.div`
+  display: flex;
   width: 100%;
+  gap: 24px;
+  height: 65%;
+  justify-content: center;
+`;
+
+const OrderColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  gap: 10px;
+  overflow: auto;
+  scrollbar-width: none;
+`;
+
+const OrderHeader = styled.div`
+  background: white;
+  position: sticky;
+  top: 0;
+  width: 350px;
+  height: 110px;
+  flex-basis: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+`;
+
+const ProductsByOrderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+`;
+
+const ProductsByOrderSubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  height: 100%;
+  gap: 10px;
+  background: #f8f8f8;
 `;
 
 function AdminOrder() {
@@ -35,21 +88,44 @@ function AdminOrder() {
 
   return (
     <Container>
-      <div>
-        {notPaidOrders.map((it) => (
-          <NotPaidOrderCard order={it} key={it.id} />
-        ))}
-      </div>
-      <div>
-        {paidOrders.map((it) => (
-          <PaidOrderCard order={it} key={it.id} />
-        ))}
-      </div>
-      <div>
-        {servedOrders.map((it) => (
-          <ServedOrderCard order={it} key={it.id} />
-        ))}
-      </div>
+      <TitleNavBar title={'실시간 주문 조회'} />
+      <KanbanContainer>
+        <OrderColumnContainer>
+          <OrderHeader>
+            <AppLabel size={22} style={{ fontWeight: 600 }}>
+              결제 대기
+            </AppLabel>
+          </OrderHeader>
+          {notPaidOrders.map((it) => (
+            <NotPaidOrderCard order={it} key={it.id} />
+          ))}
+        </OrderColumnContainer>
+        <VerticalDivider />
+        <OrderColumnContainer>
+          <OrderHeader>
+            <AppLabel size={22} style={{ fontWeight: 600 }}>
+              결제 완료
+            </AppLabel>
+          </OrderHeader>
+          {paidOrders.map((it) => (
+            <PaidOrderCard order={it} key={it.id} />
+          ))}
+        </OrderColumnContainer>
+        <VerticalDivider />
+        <OrderColumnContainer>
+          <OrderHeader>
+            <AppLabel size={22} style={{ fontWeight: 600 }}>
+              서빙 완료
+            </AppLabel>
+          </OrderHeader>
+          {servedOrders.map((it) => (
+            <ServedOrderCard order={it} key={it.id} />
+          ))}
+        </OrderColumnContainer>
+      </KanbanContainer>
+      <ProductsByOrderContainer>
+        <ProductsByOrderSubContainer> </ProductsByOrderSubContainer>
+      </ProductsByOrderContainer>
     </Container>
   );
 }
