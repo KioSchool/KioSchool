3e2035cb663125dd0b3a6c16b5b260bab70f6307
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-05-07 16:54:35 +0900

    Edit implement products count feature

--- src/pages/Admin/order/AdminOrder.tsx
+++ src/pages/Admin/order/AdminOrder.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useOrdersWebsocket from '@hooks/user/useOrdersWebsocket';
 import { useRecoilValue } from 'recoil';
-import { ordersAtom } from '@recoils/atoms';
+import { ordersAtom, productsAtom } from '@recoils/atoms';
 import PaidOrderCard from '@components/admin/order/PaidOrderCard';
 import NotPaidOrderCard from '@components/admin/order/NotPaidOrderCard';
 import ServedOrderCard from '@components/admin/order/ServedOrderCard';
@@ -11,6 +11,8 @@ import styled from '@emotion/styled';
 import TitleNavBar from '@components/common/nav/TitleNavBar';
 import VerticalDivider from '@components/common/divider/VerticalDivider';
 import AppLabel from '@components/common/label/AppLabel';
+import _ from 'lodash';
+import useAdminProducts from '@hooks/admin/useAdminProducts';
 
 const Container = styled.div`
   display: flex;
@@ -69,22 +71,68 @@ const ProductsByOrderSubContainer = styled.div`
   height: 100%;
   gap: 10px;
   background: #f8f8f8;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
+const ProductsContainer = styled.div`
+  display: flex;
+  height: 100%;
+  gap: 10px;
+  overflow: auto;
+`;
+
+const ProductCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 150px;
+  padding: 5px;
+  box-sizing: border-box;
+  background: white;
+  border-radius: 10px;
+  gap: 5px;
+`;
+
+const ProductImage = styled.img`
+  border-radius: 10px;
+  width: 80px;
+  height: 80px;
+  object-fit: fill;
 `;
 
 function AdminOrder() {
   const { workspaceId } = useParams<{ workspaceId: string }>();
   const { subscribeOrders } = useOrdersWebsocket(workspaceId);
   const { fetchTodayOrders } = useAdminOrder(workspaceId);
+  const { fetchProducts } = useAdminProducts(workspaceId);
+
+  const products = useRecoilValue(productsAtom);
   const orders = useRecoilValue(ordersAtom);
 
   useEffect(() => {
     subscribeOrders();
     fetchTodayOrders();
+    fetchProducts();
   }, []);
 
   const notPaidOrders = orders.filter((it) => it.status === 'NOT_PAID');
   const paidOrders = orders.filter((it) => it.status === 'PAID');
   const servedOrders = orders.filter((it) => it.status === 'SERVED');
+
+  const productCounts = _.mapValues(
+    _.groupBy(
+      _.flatMap(
+        orders.filter((it) => it.status == 'PAID'),
+        'orderProducts',
+      ).filter((it) => !it.isServed),
+      'product.id',
+    ),
+    (filteredProducts) => _.sumBy(filteredProducts, 'quantity'),
+  );
+  const productMap = _.keyBy(products, 'id');
 
   return (
     <Container>
@@ -124,7 +172,24 @@ function AdminOrder() {
         </OrderColumnContainer>
       </KanbanContainer>
       <ProductsByOrderContainer>
-        <ProductsByOrderSubContainer> </ProductsByOrderSubContainer>
+        <ProductsByOrderSubContainer>
+          <AppLabel size={23} style={{ fontWeight: 600 }}>
+            결제 완료 주문 상품별 현황
+          </AppLabel>
+          <ProductsContainer>
+            {_.keys(productCounts).map((productId) => (
+              <ProductCard key={productId}>
+                <ProductImage src={productMap[productId].imageUrl} />
+                <AppLabel key={productId} size={14} style={{ fontWeight: 600 }}>
+                  {productMap[productId].name}
+                </AppLabel>
+                <AppLabel key={productId} size={12}>
+                  {productCounts[productId]}개
+                </AppLabel>
+              </ProductCard>
+            ))}
+          </ProductsContainer>
+        </ProductsByOrderSubContainer>
       </ProductsByOrderContainer>
     </Container>
   );
