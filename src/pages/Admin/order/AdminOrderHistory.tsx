335b83b05d7a19d4ab542301b8a97fb97c5cde17
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-05-10 17:21:02 +0900

    Edit AdminOrderHistory page to apply design

--- src/pages/Admin/order/AdminOrderHistory.tsx
+++ src/pages/Admin/order/AdminOrderHistory.tsx
@@ -6,6 +6,11 @@ import useAdminOrder from '@hooks/admin/useAdminOrder';
 import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { ordersAtom } from '@recoils/atoms';
+import AppContainer from '@components/common/container/AppContainer';
+import ToggleOrderCard from '@components/admin/order/ToggleOrderCard';
+import TitleNavBar from '@components/common/nav/TitleNavBar';
+import AppLabel from '@components/common/label/AppLabel';
+import AppCheckBox from '@components/common/input/AppCheckBox';
 
 const Container = styled.div`
   padding: 50px;
@@ -13,29 +18,85 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const ConditionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DatePickerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 24px;
+  margin-bottom: 24px;
+`;
+
+const OrderStatusConditionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 24px;
+`;
+
+const OrderCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  gap: 10px;
+  overflow: auto;
+`;
+
+const TotalPriceContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 10px;
+`;
+
 function AdminOrderHistory() {
   const { workspaceId } = useParams<{ workspaceId: string }>();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [showServedOrder, setShowServedOrder] = useState(false);
   const { fetchOrders } = useAdminOrder(workspaceId);
-  const orders = useRecoilValue(ordersAtom);
+  const orders = showServedOrder ? useRecoilValue(ordersAtom).filter((order) => order.status === 'SERVED') : useRecoilValue(ordersAtom);
+
+  const totalOrderPrice = orders.reduce((acc, cur) => acc + cur.totalPrice, 0).toLocaleString();
 
   useEffect(() => {
     fetchOrders({ startDate: startDate.toISOString().split('T')[0], endDate: endDate.toISOString().split('T')[0] });
   }, [startDate, endDate]);
 
   return (
-    <Container>
-      <ReactDatePicker selected={startDate} shouldCloseOnSelect dateFormat={'yyyy.MM.dd'} onChange={(date: Date) => setStartDate(date)} />
-      <ReactDatePicker selected={endDate} shouldCloseOnSelect dateFormat={'yyyy.MM.dd'} onChange={(date: Date) => setEndDate(date)} />
-      <div>
-        {orders.map((it) => (
-          <div key={it.id}>
-            {it.id} - {it.totalPrice.toLocaleString()}원 - {it.status} - {it.createdAt}
-          </div>
-        ))}
-      </div>
-    </Container>
+    <AppContainer justifyValue={'center'}>
+      <Container>
+        <TitleNavBar title={'전체 주문 조회'} />
+        <ConditionContainer>
+          <DatePickerContainer>
+            <ReactDatePicker selected={startDate} shouldCloseOnSelect dateFormat={'yyyy.MM.dd'} onChange={(date: Date) => setStartDate(date)} />
+            {'~'}
+            <ReactDatePicker selected={endDate} shouldCloseOnSelect dateFormat={'yyyy.MM.dd'} onChange={(date: Date) => setEndDate(date)} />
+          </DatePickerContainer>
+          <OrderStatusConditionContainer>
+            <AppCheckBox
+              checked={showServedOrder}
+              onChange={() => {
+                setShowServedOrder((prev) => {
+                  return !prev;
+                });
+              }}
+              label={'서빙 완료 주문만 보기'}
+            />
+          </OrderStatusConditionContainer>
+        </ConditionContainer>
+        <OrderCardContainer>
+          {orders.map((order) => (
+            <ToggleOrderCard key={order.id} order={order} />
+          ))}
+        </OrderCardContainer>
+        <TotalPriceContainer>
+          <AppLabel size={25}>총 주문 금액: {totalOrderPrice}원</AppLabel>
+        </TotalPriceContainer>
+      </Container>
+    </AppContainer>
   );
 }
 
