9c579e9f125496ce2f68d4fd73d519718345dceb
Rune <sungjong5k@naver.com>
2024-05-15 16:40:00 +0900

    Add ManageAccountInfo

--- src/pages/Admin/Info.tsx
+++ src/pages/Admin/Info.tsx
@@ -1,9 +1,22 @@
+import ManageAccount from '@components/admin/info/ManageAccount';
 import AppContainer from '@components/common/container/AppContainer';
+import styled from '@emotion/styled';
+
+const ContentContainer = styled.div`
+  padding-top: 100px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  justify-direction: column;
+`;
 
 function Info() {
   return (
-    <AppContainer justifyValue={'center'} alignItems={'center'} flexDirection={'column'}>
-      <p>hi</p>
+    <AppContainer justifyValue={'center'} alignItems={'center'} fullWidth={true}>
+      <ContentContainer>
+        <ManageAccount />
+      </ContentContainer>
     </AppContainer>
   );
 }
