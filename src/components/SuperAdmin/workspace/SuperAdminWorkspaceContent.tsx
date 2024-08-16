03178f8b5c5a49a5d023d78931ca513edc7368bf
Rune <sungjong5k@naver.com>
2024-08-16 16:26:33 +0900

    Add Label style

--- src/components/SuperAdmin/workspace/SuperAdminWorkspaceContent.tsx
+++ src/components/SuperAdmin/workspace/SuperAdminWorkspaceContent.tsx
@@ -1,12 +1,24 @@
 import { Workspace } from '@@types/index';
 import { SubContainer } from '@components/common/container/AppContainer';
+import AppLabel from '@components/common/label/AppLabel';
+import styled from '@emotion/styled';
+
+const SubLabelContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #d8d8d8;
+`;
 
 function SuperAdminWorkspaceContent({ name, owner, createdAt }: Workspace) {
+  const datePart = createdAt.split('T')[0];
+  const filteredCreatedDate = datePart.replace(/-/g, '.');
+
   return (
-    <SubContainer justifyValue="flex-start" customWidth="1000px" customHeight="80px">
-      {name}
-      {owner.name}
-      {createdAt}
+    <SubContainer style={{ gap: '5px' }} flexDirection="column" justifyValue="center" alignItems="flex-start" customWidth="1000px" customHeight="80px">
+      <AppLabel size={20} style={{ fontWeight: 600 }}>
+        {name}
+      </AppLabel>
+      <SubLabelContainer>{`${filteredCreatedDate} | ${owner.name}`}</SubLabelContainer>
     </SubContainer>
   );
 }
