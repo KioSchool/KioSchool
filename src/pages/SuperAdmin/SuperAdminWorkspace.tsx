c30c7e625789675abfc113b3c5f03a88fca0e102
Rune <sungjong5k@naver.com>
2024-08-16 16:37:09 +0900

    Add styled hr after SuperAdminWorkspaceContent

--- src/pages/SuperAdmin/SuperAdminWorkspace.tsx
+++ src/pages/SuperAdmin/SuperAdminWorkspace.tsx
@@ -1,10 +1,16 @@
 import AppContainer from '@components/common/container/AppContainer';
 import TitleNavBar from '@components/common/nav/TitleNavBar';
 import SuperAdminWorkspaceContent from '@components/SuperAdmin/workspace/SuperAdminWorkspaceContent';
+import styled from '@emotion/styled';
 import useSuperAdminWorkspace from '@hooks/SuperAdmin/useSuperAdminWorkspace';
 import { userWorkspaceListAtom } from '@recoils/atoms';
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
+
+const HorizontalLine = styled.hr`
+  width: 100%;
+  border: 0.3px solid #d8d8d8;
+`;
 
 function SuperAdminWorkspace() {
   const { fetchAllWorkspaces } = useSuperAdminWorkspace();
@@ -18,9 +24,14 @@ function SuperAdminWorkspace() {
     <AppContainer justifyValue="center" customWidth="1000px">
       <>
         <TitleNavBar title="전체 워크스페이스 관리" />
-        {workspaces.content.map((item) => {
+        {workspaces.content.map((item, index) => {
           console.log(item);
-          return <SuperAdminWorkspaceContent {...item} key={item.id} />;
+          return (
+            <>
+              <SuperAdminWorkspaceContent {...item} key={item.id} />
+              {index < workspaces.content.length - 1 && <HorizontalLine />}
+            </>
+          );
         })}
       </>
     </AppContainer>
