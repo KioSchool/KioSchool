9e42969da0516c30ba320fe83fb6a56d1bfaf7c8
Rune <sungjong5k@naver.com>
2024-08-16 22:52:38 +0900

    Add workspace detail contents and link button

--- src/components/SuperAdmin/workspace/SuperAdminWorkspaceContent.tsx
+++ src/components/SuperAdmin/workspace/SuperAdminWorkspaceContent.tsx
@@ -1,7 +1,9 @@
 import { Workspace } from '@@types/index';
+import AppButton from '@components/common/button/AppButton';
 import { SubContainer } from '@components/common/container/AppContainer';
-import AppLabel from '@components/common/label/AppLabel';
 import styled from '@emotion/styled';
+import { navBarLabelStyle } from '@styles/navBarStyles';
+import { useNavigate } from 'react-router-dom';
 
 const SubLabelContainer = styled.div`
   display: flex;
@@ -9,16 +11,43 @@ const SubLabelContainer = styled.div`
   color: #d8d8d8;
 `;
 
-function SuperAdminWorkspaceContent({ name, owner, createdAt }: Workspace) {
+const WorkspaceLabel = styled.div`
+  ${navBarLabelStyle}
+`;
+
+interface WorkspaceProps extends Workspace {
+  showDescribe: boolean;
+  onSelect: (id: number) => void;
+}
+
+function SuperAdminWorkspaceContent({ id, name, owner, createdAt, showDescribe, onSelect }: WorkspaceProps) {
   const datePart = createdAt.split('T')[0];
   const filteredCreatedDate = datePart.replace(/-/g, '.');
+  const navigate = useNavigate();
 
   return (
     <SubContainer flexDirection="column" justifyValue="center" alignItems="flex-start" customWidth="1000px" customHeight="80px" customGap="5px">
-      <AppLabel size={20} style={{ fontWeight: 600, cursor: 'pointer' }}>
+      <WorkspaceLabel
+        onClick={() => {
+          onSelect(id);
+        }}
+      >
         {name}
-      </AppLabel>
+      </WorkspaceLabel>
+
       <SubLabelContainer>{`${filteredCreatedDate} | ${owner.name}`}</SubLabelContainer>
+      {showDescribe && (
+        <>
+          <div>Detail Contents</div>
+          <AppButton
+            onClick={() => {
+              navigate(`/admin/workspace/${id}`);
+            }}
+          >
+            바로가기
+          </AppButton>
+        </>
+      )}
     </SubContainer>
   );
 }
--- src/pages/SuperAdmin/SuperAdminWorkspace.tsx
+++ src/pages/SuperAdmin/SuperAdminWorkspace.tsx
@@ -5,6 +5,7 @@ import SuperAdminSearchBar from '@components/SuperAdmin/workspace/SuperAdminSear
 import SuperAdminWorkspaceContent from '@components/SuperAdmin/workspace/SuperAdminWorkspaceContent';
 import styled from '@emotion/styled';
 import { userWorkspaceListAtom } from '@recoils/atoms';
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 const HorizontalLine = styled.hr`
@@ -22,6 +23,11 @@ const ContentContainer = styled.div`
 
 function SuperAdminWorkspace() {
   const workspaces = useRecoilValue(userWorkspaceListAtom);
+  const [selectedWorkspaceId, setSelectedWorkspaceId] = useState<number>(0);
+
+  const handleSelectWorkspace = (id: number) => {
+    setSelectedWorkspaceId((prevId) => (prevId === id ? 0 : id));
+  };
 
   return (
     <AppContainer justifyValue="center" customWidth="1000px" customHeight="100%" customGap="20px">
@@ -29,14 +35,12 @@ function SuperAdminWorkspace() {
         <TitleNavBar title="전체 워크스페이스 관리" />
         <SuperAdminSearchBar />
         <ContentContainer>
-          {workspaces.content.map((item, index) => {
-            return (
-              <>
-                <SuperAdminWorkspaceContent {...item} key={item.id} />
-                {index < workspaces.content.length - 1 && <HorizontalLine key={index} />}
-              </>
-            );
-          })}
+          {workspaces.content.map((item, index) => (
+            <div key={item.id}>
+              <SuperAdminWorkspaceContent {...item} showDescribe={selectedWorkspaceId === item.id} onSelect={handleSelectWorkspace} />
+              {index < workspaces.content.length - 1 && <HorizontalLine />}
+            </div>
+          ))}
         </ContentContainer>
         <Pagination totalPageCount={workspaces.totalPages - 1} />
       </>
