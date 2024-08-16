05705cfb71c12aa4d6f9c680049748d007decbbf
Rune <sungjong5k@naver.com>
2024-08-16 15:38:18 +0900

    Add userWorkspaceListAtom

--- src/recoils/atoms.ts
+++ src/recoils/atoms.ts
@@ -1,5 +1,5 @@
 import { atom } from 'recoil';
-import { Order, OrderProductBase, Product, ProductCategory, User, Workspace } from '@@types/index';
+import { Order, OrderProductBase, Product, ProductCategory, User, Workspace, WorkspaceList } from '@@types/index';
 
 export const ordersAtom = atom<Order[]>({
   key: 'ordersAtom',
@@ -79,4 +79,36 @@ export const orderBasketAtom = atom<OrderProductBase[]>({
 export const isLoadingAtom = atom<boolean>({
   key: 'isLoadingAtom',
   default: false,
+});
+
+export const userWorkspaceListAtom = atom<WorkspaceList>({
+  key: 'userWorkspaceListAtom',
+  default: {
+    content: [],
+    pageable: {
+      pageNumber: 0,
+      pageSize: 6,
+      sort: {
+        sorted: false,
+        empty: true,
+        unsorted: true,
+      },
+      offset: 0,
+      paged: true,
+      unpaged: false,
+    },
+    totalPages: 0,
+    totalElements: 0,
+    last: false,
+    number: 0,
+    size: 6,
+    numberOfElements: 0,
+    sort: {
+      sorted: false,
+      empty: true,
+      unsorted: true,
+    },
+    first: true,
+    empty: true,
+  },
 });
