bf131f6cbcaeb15cac12d47d9f366808c5fc2431
Rune <sungjong5k@naver.com>
2024-08-24 00:03:04 +0900

    Fix AppContainer width fiedl from 100vw to 100%
    세로 스크롤바가 생기는 경우, 100vw는 스크롤바를 고려하지 않기 때문에 가로 스크롤바가 생깁니다. 그래서 100%로 바꾸었습니다.

--- src/components/common/container/AppContainer.tsx
+++ src/components/common/container/AppContainer.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import NavBar from '@components/common/nav/NavBar';
 
 export const MainContainer = styled.div<{ backgroundColor?: string }>`
-  width: 100vw;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
