1ee0c06eaaed6c497ea9c9532053489c52efbcad
Rune <sungjong5k@naver.com>
2024-08-24 14:39:36 +0900

    Add Params type for fetchAllWorkspaces

--- src/hooks/SuperAdmin/useSuperAdminWorkspace.tsx
+++ src/hooks/SuperAdmin/useSuperAdminWorkspace.tsx
@@ -3,13 +3,24 @@ import useApi from '@hooks/useApi';
 import { userWorkspaceListAtom } from '@recoils/atoms';
 import { useSetRecoilState } from 'recoil';
 
+interface ParamsType {
+  page: number;
+  size: number;
+  name?: string;
+}
+
 function useSuperAdminWorkspace() {
   const { superAdminApi } = useApi();
   const setUserWorkspaceList = useSetRecoilState(userWorkspaceListAtom);
 
   const fetchAllWorkspaces = (page: number, size: number, name?: string) => {
+    const params: ParamsType = { page, size };
+    if (name) {
+      params.name = name;
+    }
+
     superAdminApi
-      .get<WorkspaceList>('/workspaces', { params: { page, size, name } })
+      .get<WorkspaceList>('/workspaces', { params: params })
       .then((res) => {
         setUserWorkspaceList(res.data);
       })
