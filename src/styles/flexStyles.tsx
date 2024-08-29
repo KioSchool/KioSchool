653577e07a5ace84a7669ed0430413ff741822cf
Rune <sungjong5k@naver.com>
2024-08-29 18:26:22 +0900

    Edit flexStyleProps

--- src/styles/flexStyles.tsx
+++ src/styles/flexStyles.tsx
@@ -1,14 +1,14 @@
 import { css } from '@emotion/react';
 
 interface FlexStyleProps {
-  direction: string;
+  direction?: string;
   justify?: string;
   align?: string;
 }
 
-export const flexStyle = ({ direction = 'column', justify, align }: FlexStyleProps) => css`
+export const flexStyle = ({ direction, justify, align }: FlexStyleProps) => css`
   display: flex;
-  flex-direction: ${direction};
-  justify-content: ${justify || 'center'};
-  align-items: ${align || 'center'};
+  flex-direction: ${direction || 'column'};
+  justify-content: ${justify};
+  align-items: ${align};
 `;
