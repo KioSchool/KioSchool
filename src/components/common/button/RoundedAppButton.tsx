818b0abce0d481e9b36267364249cf6563cacf0f
Rune <sungjong5k@naver.com>
2024-09-09 21:03:18 +0900

    Add RoundedAppButton

---
+++ src/components/common/button/RoundedAppButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export interface RoundedAppButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: string;
+}
+
+const Container = styled.button`
+  width: ${(props: RoundedAppButtonProps) => props.size};
+  background: #eb6d09;
+  color: white;
+  font-size: 15px;
+  border: none;
+  border-radius: 20px;
+  height: 35px;
+  padding: 0 35px;
+  user-select: none;
+  &:hover {
+    background: #ff7b2b;
+  }
+  &:disabled {
+    background: #b1b1b1;
+  }
+`;
+
+function RoundedAppButton(props: RoundedAppButtonProps) {
+  return <Container {...props} className={'rounded-app-button'} />;
+}
+
+export default RoundedAppButton;
