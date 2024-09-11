e5edc036b2205579b904b2ee91f872c61d64f83d
Rune <sungjong5k@naver.com>
2024-09-11 15:56:38 +0900

    Change SuperAdminHome component

--- src/pages/SuperAdmin/SuperAdminHome.tsx
+++ src/pages/SuperAdmin/SuperAdminHome.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import AppContainer from '@components/common/container/AppContainer';
+import TestContainer from '@components/common/container/TestContainer';
 import styled from '@emotion/styled';
 import ArrowRight from '@resources/svg/ArrowRightSvg';
 import { css } from '@emotion/react';
 import AppFooter from '@components/common/footer/AppFooter';
-import { rowFlex } from '@styles/flexStyles';
+import { colFlex, rowFlex } from '@styles/flexStyles';
 
 const MainTitle = styled.div`
   width: 540px;
@@ -74,7 +74,7 @@ function SuperAdminHome() {
   const [isHover, setIsHover] = useState<boolean>(false);
 
   return (
-    <AppContainer contentsJustify={'center'} contentsDirection={'column'} contentsAlign={'flex-start'}>
+    <TestContainer useFlex={colFlex({ justify: 'center', align: 'start' })}>
       <>
         <MainTitle className={'main-title'}>
           키오스쿨로
@@ -94,7 +94,7 @@ function SuperAdminHome() {
         <AppFooter />
         <HoverOverlay isHover={isHover} className={'hover-overlay'} />
       </>
-    </AppContainer>
+    </TestContainer>
   );
 }
 
