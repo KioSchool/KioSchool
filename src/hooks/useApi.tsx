24b98a9290bf5987362135a624ba15458781b896
Rune <sungjong5k@naver.com>
2024-09-11 23:34:46 +0900

    Hide loading status when request responds within 500 ms

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -3,14 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 import { isLoadingAtom } from '@recoils/atoms';
 
-interface UseApiProps {
-  useLoading?: boolean;
-}
-
-function useApi({ useLoading = true }: UseApiProps = {}) {
+function useApi() {
   const navigate = useNavigate();
   const setIsLoading = useSetRecoilState(isLoadingAtom);
   const controller = new AbortController();
+  const map = new Map();
 
   const adminApi = axios.create({
     baseURL: process.env.REACT_APP_ENVIRONMENT == 'development' ? 'http://localhost:8080/admin' : 'https://kio-school.fly.dev/admin',
@@ -19,23 +16,37 @@ function useApi({ useLoading = true }: UseApiProps = {}) {
   });
 
   const commonRequestInterceptor = (config: any) => {
-    if (useLoading) setIsLoading(true);
+    const timeOutId = setTimeout(() => {
+      setIsLoading(true);
+    }, 500);
+
+    map.set(config, timeOutId);
+
     return config;
   };
 
   const commonResponseInterceptor = (response: any) => {
-    if (useLoading) setIsLoading(false);
+    const timeOutId = map.get(response.config);
+    clearTimeout(timeOutId);
+    setIsLoading(false);
+
     return response;
   };
 
   const commonErrorInterceptor = (error: any) => {
-    if (useLoading) setIsLoading(false);
+    const timeOutId = map.get(error.config);
+    clearTimeout(timeOutId);
+    setIsLoading(false);
+
     return Promise.reject(error);
   };
 
   adminApi.interceptors.request.use(commonRequestInterceptor, commonErrorInterceptor);
   adminApi.interceptors.response.use(commonResponseInterceptor, (error) => {
-    if (useLoading) setIsLoading(false);
+    const timeOutId = map.get(error.config);
+    clearTimeout(timeOutId);
+    setIsLoading(false);
+
     if (error.response.status === 403) {
       controller.abort();
       alert('로그인이 필요합니다.');
@@ -61,7 +72,10 @@ function useApi({ useLoading = true }: UseApiProps = {}) {
 
   superAdminApi.interceptors.request.use(commonRequestInterceptor, commonErrorInterceptor);
   superAdminApi.interceptors.response.use(commonResponseInterceptor, (error) => {
-    if (useLoading) setIsLoading(false);
+    const timeOutId = map.get(error.config);
+    clearTimeout(timeOutId);
+    setIsLoading(false);
+
     if (error.response.status === 403) {
       controller.abort();
       alert('권한이 없습니다.');
