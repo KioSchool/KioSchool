fff9f851ad32ee22240f510e4d9f83eaba50fbf9
Rune <sungjong5k@naver.com>
2024-09-12 23:12:10 +0900

    Add onLeftArrwClick function for navigation and change atom

--- src/pages/SuperAdmin/SuperAdminWorkspace.tsx
+++ src/pages/SuperAdmin/SuperAdminWorkspace.tsx
@@ -2,13 +2,14 @@ import AppContainer from '@components/common/container/AppContainer';
 import Pagination from '@components/common/pagination/Pagination';
 import SuperAdminSearchBar from '@components/SuperAdmin/workspace/SuperAdminSearchBar';
 import styled from '@emotion/styled';
-import { userPaginationResponseAtom } from '@recoils/atoms';
+import { workspacePaginationResponseAtom } from '@recoils/atoms';
 import { useEffect, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 import SuperAdminSearchContents from './SuperAdminSearchContents';
 import useSuperAdminWorkspace from '@hooks/SuperAdmin/useSuperAdminWorkspace';
 import { colFlex } from '@styles/flexStyles';
 import SuperAdminWorkspaceContent from '@components/SuperAdmin/workspace/SuperAdminWorkspaceContent';
+import { useNavigate } from 'react-router-dom';
 
 const ContentContainer = styled.div<{ justifyCenter?: boolean }>`
   height: 550px;
@@ -20,10 +21,11 @@ const ContentContainer = styled.div<{ justifyCenter?: boolean }>`
 `;
 
 function SuperAdminWorkspace() {
-  const userInputRef = useRef<HTMLInputElement | null>(null);
-  const workspaces = useRecoilValue(userPaginationResponseAtom);
-  const { fetchAllWorkspaces } = useSuperAdminWorkspace();
   const size = 6;
+  const navigate = useNavigate();
+  const userInputRef = useRef<HTMLInputElement | null>(null);
+  const workspaces = useRecoilValue(workspacePaginationResponseAtom);
+  const { fetchAllWorkspaces } = useSuperAdminWorkspace();
   const emptyWorkspaces = workspaces.numberOfElements === 0;
 
   useEffect(() => {
@@ -36,7 +38,7 @@ function SuperAdminWorkspace() {
       customWidth={'1000px'}
       customHeight={'100%'}
       customGap={'20px'}
-      titleNavBarProps={{ title: '전체 워크스페이스 관리' }}
+      titleNavBarProps={{ title: '전체 워크스페이스 관리', onLeftArrowClick: () => navigate('/super-admin/manage') }}
     >
       <>
         <SuperAdminSearchBar ref={userInputRef} fetchContents={fetchAllWorkspaces} />
