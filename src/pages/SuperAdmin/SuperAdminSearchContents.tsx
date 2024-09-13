50429490d64d6845d17f8795539ffb29f0003c65
Rune <sungjong5k@naver.com>
2024-09-13 16:00:44 +0900

    Change checking empty code for clarity

--- src/pages/SuperAdmin/SuperAdminSearchContents.tsx
+++ src/pages/SuperAdmin/SuperAdminSearchContents.tsx
@@ -18,7 +18,7 @@ interface ContentsProps {
 }
 
 function SuperAdminSearchContents({ contents, target, ContentComponent }: ContentsProps) {
-  if (contents.numberOfElements === 0) {
+  if (contents.empty) {
     return <EmptyLabel className={'empty-label'}>{`찾고자 하는 ${target} 이/가 존재하지 않습니다.`}</EmptyLabel>;
   }
 
