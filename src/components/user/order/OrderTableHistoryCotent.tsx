7e8e479caacef69b72a3082d1acdea236df65e1f
Rune <sungjong5k@naver.com>
2024-10-07 17:07:19 +0900

    Add OrderTableHistoryContent contents

--- src/components/user/order/OrderTableHistoryCotent.tsx
+++ src/components/user/order/OrderTableHistoryCotent.tsx
@@ -1,5 +1,43 @@
-function OrderTableHistoryContent() {
-  return <h1>hi</h1>;
+import { Table } from '@@types/index';
+import { SubContainer } from '@components/common/container/AppContainer';
+import styled from '@emotion/styled';
+import { Color } from '@resources/colors';
+import { colFlex, rowFlex } from '@styles/flexStyles';
+
+const SubLabelContainer = styled.div`
+  color: #d8d8d8;
+  ${rowFlex()}
+`;
+
+const TableOrderLabel = styled.div`
+  text-align: center;
+  font-size: 18px;
+  font-weight: 400;
+  text-decoration: none;
+  color: ${Color.GREY};
+  cursor: pointer;
+  transition: ease-in 0.1s;
+  &:hover {
+    color: ${Color.KIO_ORANGE};
+    text-decoration: underline;
+  }
+`;
+
+function OrderTableHistoryContent(props: Table) {
+  const datePart = props.createdAt.split('T')[0];
+  const timePart = props.createdAt.split('T')[1].split(':');
+  const filteredCreatedDate = datePart.replace(/-/g, '.') + ' - ' + timePart[0] + ':' + timePart[1];
+
+  const createdDateAndOwnerText = `${props.customerName} | ${filteredCreatedDate}`;
+
+  return (
+    <SubContainer useFlex={colFlex({ justify: 'center', align: 'start' })} customWidth={'1000px'} customHeight={'80px'} customGap={'5px'}>
+      <TableOrderLabel onClick={() => {}} className={'table-order-label'}>
+        {`주문번호 ${props.id}번`}
+      </TableOrderLabel>
+      <SubLabelContainer className={'sub-label-container'}>{createdDateAndOwnerText}</SubLabelContainer>
+    </SubContainer>
+  );
 }
 
 export default OrderTableHistoryContent;
