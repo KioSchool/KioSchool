3155a74307c8d8f38365e9b250c476aa0a3c664c
Rune <sungjong5k@naver.com>
2024-10-07 14:32:46 +0900

    Add AdminOrderCheck component

--- src/pages/Admin/order/AdminOrderCheck.tsx
+++ src/pages/Admin/order/AdminOrderCheck.tsx
@@ -1,5 +1,38 @@
+import ImageRouteButton from '@components/common/button/ImageRouteButton';
+import orderImage from '@resources/image/orderImage.png';
+import orderHistoryImage from '@resources/image/orderHistoryImage.png';
+import orderManageImage from '@resources/image/orderManageImage.png';
+import AppFooter from '@components/common/footer/AppFooter';
+import AppContainer from '@components/common/container/AppContainer';
+import { colFlex, rowFlex } from '@styles/flexStyles';
+import styled from '@emotion/styled';
+import useCustomNavigate from '@hooks/useCustomNavigate';
+
+const Container = styled.div`
+  width: 100%;
+  ${colFlex({ align: 'center' })}
+`;
+
+export const ButtonContainer = styled.div`
+  gap: 30px;
+  ${rowFlex()}
+`;
+
 function AdminOrderCheck() {
-  return <h1>hi</h1>;
+  const { replaceLastPath } = useCustomNavigate();
+
+  return (
+    <AppContainer useFlex={colFlex({ justify: 'center' })} titleNavBarProps={{ title: '주문 조회', subTitle: '주문 조회를 다양하게 관리할 수 있습니다.' }}>
+      <Container className={'admin-workspace-container'}>
+        <ButtonContainer className={'button-container'}>
+          <ImageRouteButton src={orderImage} onClick={() => replaceLastPath('/orders')} buttonText={'실시간 주문 조회'} />
+          <ImageRouteButton src={orderHistoryImage} onClick={() => replaceLastPath('/orders-history')} buttonText={'전체 주문 조회'} />
+          <ImageRouteButton src={orderManageImage} onClick={() => replaceLastPath('/oreders-table')} buttonText={'테이블별 주문 조회'} />
+        </ButtonContainer>
+        <AppFooter />
+      </Container>
+    </AppContainer>
+  );
 }
 
 export default AdminOrderCheck;
