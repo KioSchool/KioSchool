a9322bbfeeecec0247b4e0ba072efc8a87c268fe
Rune <sungjong5k@naver.com>
2025-01-06 14:29:24 +0900

    Feat: Add an useEmail custom hook

---
+++ src/hooks/user/useEmail.tsx
@@ -0,0 +1,37 @@
+import { PaginationResponse, EmailDomain } from '@@types/index';
+import { defaultPaginationValue } from '@@types/PaginationType';
+import useApi from '@hooks/useApi';
+import { useSearchParams } from 'react-router-dom';
+
+interface FetchAllEmailsParamsType {
+  page: number;
+  size: number;
+  name?: string;
+}
+
+function useEmail() {
+  const { userApi } = useApi();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const fetchAllEmails = (page: number, size: number, name?: string) => {
+    const params: FetchAllEmailsParamsType = { page, size, name };
+
+    const response = userApi
+      .get<PaginationResponse<EmailDomain>>('/email-domains', { params })
+      .then((res) => {
+        searchParams.set('page', params.page.toString());
+        setSearchParams(searchParams);
+        return res.data;
+      })
+      .catch((error) => {
+        console.error(error);
+        return defaultPaginationValue;
+      });
+
+    return response;
+  };
+
+  return { fetchAllEmails };
+}
+
+export default useEmail;
