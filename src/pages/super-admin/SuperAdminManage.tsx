64eb3c6a4c36dabe37ae6bdbe15983cafb6cf078
Rune <sungjong5k@naver.com>
2025-01-08 21:10:28 +0900

    Feat: Add useCustomNavigate hook for automatic query handling

--- src/pages/super-admin/SuperAdminManage.tsx
+++ src/pages/super-admin/SuperAdminManage.tsx
@@ -1,9 +1,9 @@
 import ImageRouteButton from '@components/common/button/ImageRouteButton';
 import AppContainer from '@components/common/container/AppContainer';
 import styled from '@emotion/styled';
+import useCustomNavigate from '@hooks/useCustomNavigate';
 import orderImage from '@resources/image/orderImage.png';
 import { colFlex, rowFlex } from '@styles/flexStyles';
-import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   width: 100%;
@@ -16,14 +16,14 @@ const ButtonContainer = styled.div`
 `;
 
 function SuperAdminManage() {
-  const navigate = useNavigate();
+  const { navigateWithPage } = useCustomNavigate();
   return (
     <AppContainer useFlex={colFlex({ justify: 'center' })} titleNavBarProps={{ title: '관리 페이지' }}>
       <Container>
         <ButtonContainer>
-          <ImageRouteButton src={orderImage} onClick={() => navigate('/super-admin/workspace?page=0')} buttonText={'워크스페이스 조회'} />
-          <ImageRouteButton src={orderImage} onClick={() => navigate('/super-admin/user?page=0')} buttonText={'사용자 조회'} />
-          <ImageRouteButton src={orderImage} onClick={() => navigate('/super-admin/email?page=0')} buttonText={'이메일 조회'} />
+          <ImageRouteButton src={orderImage} onClick={() => navigateWithPage('/super-admin/workspace')} buttonText={'워크스페이스 조회'} />
+          <ImageRouteButton src={orderImage} onClick={() => navigateWithPage('/super-admin/user')} buttonText={'사용자 조회'} />
+          <ImageRouteButton src={orderImage} onClick={() => navigateWithPage('/super-admin/email')} buttonText={'이메일 조회'} />
         </ButtonContainer>
       </Container>
     </AppContainer>
