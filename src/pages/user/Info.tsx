39bf04528cce78d30d77c26db2262b952c6e7eca
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-17 21:55:58 +0900

    Add layout of info page

--- src/pages/user/Info.tsx
+++ src/pages/user/Info.tsx
@@ -1,10 +1,21 @@
 import { colFlex } from '@styles/flexStyles';
 import AppContainer from '@components/common/container/AppContainer';
+import InfoMain from '@components/user/info/InfoMain';
+import InfoBenefit from '@components/user/info/InfoBenefit';
+import InfoStatisticAndReview from '@components/user/info/InfoStatisticAndReview';
+import InfoOurService from '@components/user/info/InfoOurService';
+import InfoCreatorsWords from '@components/user/info/InfoCreatorsWords';
 
 function Info() {
   return (
-    <AppContainer useFlex={colFlex({ align: 'center' })}>
-      <div>Info</div>
+    <AppContainer useFlex={colFlex()} customWidth={'100vw'} useScroll={true}>
+      <>
+        <InfoMain />
+        <InfoBenefit />
+        <InfoStatisticAndReview />
+        <InfoOurService />
+        <InfoCreatorsWords />
+      </>
     </AppContainer>
   );
 }
