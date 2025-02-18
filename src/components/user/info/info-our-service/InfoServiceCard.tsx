0b34d9a509638125773665a53fb31a68f677695c
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-19 02:07:57 +0900

    Add implement InfoOurService

---
+++ src/components/user/info/info-our-service/InfoServiceCard.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { colFlex } from '@styles/flexStyles';
+import { jsx } from '@emotion/react';
+import { lineSeedKrFont } from '@styles/fonts';
+import { expandButtonStyle } from '@styles/buttonStyles';
+import JSX = jsx.JSX;
+
+interface InfoServiceCardProps {
+  svg: JSX.Element;
+  title: string;
+  description: string;
+}
+
+const Container = styled.div`
+  width: 350px;
+  height: 350px;
+  border-radius: 20px;
+  box-shadow: 10px 10px 30px 0 rgba(10, 10, 10, 0.04);
+  padding: 70px 40px;
+  box-sizing: border-box;
+  ${colFlex({ justify: 'center' })};
+  ${expandButtonStyle({ scaleSize: '1.05' })};
+  cursor: default;
+`;
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 56px;
+  margin-top: 18px;
+  font-family: 'LINESeedKR-Rg', 'sans-serif';
+  ${lineSeedKrFont}
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  font-family: 'LINESeedKR-Rg', 'sans-serif';
+  color: #5b5b5b;
+  ${lineSeedKrFont}
+`;
+
+function InfoServiceCard({ svg, title, description }: InfoServiceCardProps) {
+  return (
+    <Container>
+      {svg}
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Container>
+  );
+}
+
+export default InfoServiceCard;
