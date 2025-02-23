b60812d4a66b402758017b6d91536a931e3e4659
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-24 01:37:47 +0900

    Edit separate info page pc and mobile

--- src/pages/user/info/Info.tsx
+++ src/pages/user/info/Info.tsx
@@ -1,35 +1,9 @@
-import { colFlex } from '@styles/flexStyles';
-import AppContainer from '@components/common/container/AppContainer';
-import InfoMain from '@components/user/info/info-main/InfoMain';
-import InfoBenefit from '@components/user/info/info-benefit/InfoBenefit';
-import InfoStatisticAndReview from '@components/user/info/info-statistic-and-review/InfoStatisticAndReview';
-import InfoOurService from '@components/user/info/info-our-service/InfoOurService';
-import InfoCreatorsWords from '@components/user/info/info-creators-word/InfoCreatorsWords';
-import InfoFooter from '@components/user/info/info-footer/InfoFooter';
-import styled from '@emotion/styled';
-import infoBackground from '@resources/image/infoBackground.png';
-
-const SubContainer = styled.div`
-  width: 100%;
-  background-image: url(${infoBackground});
-  background-repeat: repeat-y;
-  background-size: 100% 100%;
-  background-position: center;
-`;
+import InfoPc from '@pages/user/info/InfoPc';
+import InfoMobile from '@pages/user/info/InfoMobile';
+import { isMobile } from 'react-device-detect';
 
 function Info() {
-  return (
-    <AppContainer useFlex={colFlex()} customWidth={'100%'} useScroll={true}>
-      <SubContainer>
-        <InfoMain />
-        <InfoBenefit />
-        <InfoStatisticAndReview />
-        <InfoOurService />
-        <InfoCreatorsWords />
-        <InfoFooter />
-      </SubContainer>
-    </AppContainer>
-  );
+  return isMobile ? <InfoMobile /> : <InfoPc />;
 }
 
 export default Info;
---
+++ src/pages/user/info/InfoPc.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppContainer from '@components/common/container/AppContainer';
+import { colFlex } from '@styles/flexStyles';
+import InfoMain from '@components/user/info/info-main/InfoMain';
+import InfoBenefit from '@components/user/info/info-benefit/InfoBenefit';
+import InfoStatisticAndReview from '@components/user/info/info-statistic-and-review/InfoStatisticAndReview';
+import InfoOurService from '@components/user/info/info-our-service/InfoOurService';
+import InfoCreatorsWords from '@components/user/info/info-creators-word/InfoCreatorsWords';
+import InfoFooter from '@components/user/info/info-footer/InfoFooter';
+import styled from '@emotion/styled';
+import infoBackground from '@resources/image/infoBackground.png';
+
+const SubContainer = styled.div`
+  width: 100%;
+  background-image: url(${infoBackground});
+  background-repeat: repeat-y;
+  background-size: 100% 100%;
+  background-position: center;
+`;
+
+function InfoPc() {
+  return (
+    <AppContainer useFlex={colFlex()} customWidth={'100%'} useScroll={true}>
+      <SubContainer>
+        <InfoMain />
+        <InfoBenefit />
+        <InfoStatisticAndReview />
+        <InfoOurService />
+        <InfoCreatorsWords />
+        <InfoFooter />
+      </SubContainer>
+    </AppContainer>
+  );
+}
+
+export default InfoPc;
