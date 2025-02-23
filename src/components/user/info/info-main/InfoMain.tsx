7c40b8e9018e97604ed720cf0d42fb38bc74415f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-24 02:03:46 +0900

    Add implement mobile info main

--- src/components/user/info/info-main/InfoMain.tsx
+++ src/components/user/info/info-main/InfoMain.tsx
@@ -4,12 +4,16 @@ import { colFlex, rowFlex } from '@styles/flexStyles';
 import { Color } from '@resources/colors';
 import infoMainPhoneImage from '@resources/image/infoMainPhoneImage.png';
 import InfoMainDescription from '@components/user/info/info-main/InfoMainDescription';
+import { tabletMediaQuery } from '@styles/globalStyles';
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   user-select: none;
   ${rowFlex({ align: 'center', justify: 'center' })};
+  ${tabletMediaQuery} {
+    height: auto;
+  }
 `;
 
 const BackgroundContainer = styled.div`
@@ -18,11 +22,18 @@ const BackgroundContainer = styled.div`
   top: 25%;
   box-sizing: border-box;
   overflow: hidden;
+  ${tabletMediaQuery} {
+    top: 8%;
+  }
 `;
 
 const BackgroundText = styled.div`
   font-size: 400px;
   font-weight: 900;
+  ${tabletMediaQuery} {
+    font-size: 200px;
+    font-weight: 700;
+  }
   width: max-content;
   box-sizing: border-box;
   white-space: nowrap;
@@ -43,6 +54,10 @@ const SubContainer = styled.div`
   height: 100%;
   width: 1000px;
   padding-top: 200px;
+  ${tabletMediaQuery} {
+    width: 320px;
+    padding-top: 100px;
+  }
   box-sizing: border-box;
   z-index: 1;
   ${colFlex({ justify: 'start' })};
@@ -56,31 +71,52 @@ const TitleContainer = styled.div`
 const ContentContainer = styled.div`
   width: 100%;
   ${rowFlex({ justify: 'space-between' })};
+  ${tabletMediaQuery} {
+    ${colFlex({ justify: 'start' })};
+    gap: 24px;
+  }
 `;
 
 const DescriptionContainer = styled.div`
   width: 50%;
   height: 300px;
+  ${tabletMediaQuery} {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 const ImageContainer = styled.div`
   width: 50%;
+  ${tabletMediaQuery} {
+    width: 100%;
+  }
 `;
 
 const Image = styled.img`
   width: 140%;
   height: auto;
+  ${tabletMediaQuery} {
+    width: 100%;
+  }
 `;
 
 const MainDescription = styled.div`
   font-size: 60px;
   font-weight: 700;
+  ${tabletMediaQuery} {
+    font-size: 24px;
+  }
   color: ${Color.BLACK};
 `;
 
 const MainTitle = styled.div`
   font-size: 60px;
   font-weight: 800;
+  ${tabletMediaQuery} {
+    font-size: 32px;
+    font-weight: 700;
+  }
   color: ${Color.KIO_ORANGE};
 `;
 
--- src/pages/user/info/InfoMobile.tsx
+++ src/pages/user/info/InfoMobile.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { colFlex } from '@styles/flexStyles';
+import InfoMain from '@components/user/info/info-main/InfoMain';
 
 const Container = styled.div`
   width: 100%;
+  box-sizing: border-box;
   ${colFlex()}
 `;
 
 function InfoMobile() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <InfoMain />
+    </Container>
+  );
 }
 
 export default InfoMobile;
