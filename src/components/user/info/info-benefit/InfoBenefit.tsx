3f6af12df2a5d16a62a3273feb6f717cdecd3079
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-24 02:25:17 +0900

    Edit apply mobile style to info benefit

--- src/components/user/info/info-benefit/InfoBenefit.tsx
+++ src/components/user/info/info-benefit/InfoBenefit.tsx
@@ -4,11 +4,15 @@ import InfoTitle from '@components/user/info/InfoTitle';
 import { colFlex, rowFlex } from '@styles/flexStyles';
 import InfoBenefitFeatureList from '@components/user/info/info-benefit/InfoBenefitFeatureList';
 import InfoBenefitImage from '@components/user/info/info-benefit/InfoBenefitImage';
+import { tabletMediaQuery } from '@styles/globalStyles';
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   ${colFlex({ align: 'center' })};
+  ${tabletMediaQuery} {
+    height: auto;
+  }
 `;
 
 const SubContainer = styled.div`
@@ -17,6 +21,10 @@ const SubContainer = styled.div`
   padding-top: 120px;
   gap: 100px;
   ${colFlex()};
+  ${tabletMediaQuery} {
+    width: 320px;
+    gap: 50px;
+  }
 `;
 
 const ContentContainer = styled.div`
@@ -24,16 +32,30 @@ const ContentContainer = styled.div`
   height: 350px;
   gap: 20px;
   ${rowFlex({ justify: 'space-between' })};
+  ${tabletMediaQuery} {
+    ${colFlex()};
+    flex-direction: column-reverse;
+    height: auto;
+    gap: 50px;
+  }
 `;
 
 const FeatureContainer = styled.div`
   width: 50%;
   height: 100%;
+  ${tabletMediaQuery} {
+    width: auto;
+    height: auto;
+  }
 `;
 
 const ImageContainer = styled.div`
   width: 50%;
   height: 100%;
+  ${tabletMediaQuery} {
+    width: 100%;
+    height: auto;
+  }
 `;
 
 function InfoBenefit() {
