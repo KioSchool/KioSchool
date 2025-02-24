95acc6dda509c96aae1a15887cbce1cb3e3f747b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-24 13:12:18 +0900

    Add implement InfoCreatorsWordsMobile

--- src/pages/user/info/InfoMobile.tsx
+++ src/pages/user/info/InfoMobile.tsx
@@ -5,6 +5,7 @@ import InfoMain from '@components/user/info/info-main/InfoMain';
 import InfoBenefit from '@components/user/info/info-benefit/InfoBenefit';
 import InfoStatisticAndReview from '@components/user/info/info-statistic-and-review/InfoStatisticAndReview';
 import InfoOurService from '@components/user/info/info-our-service/InfoOurService';
+import InfoCreatorsWordsMobile from '@components/user/info/info-creators-word/InfoCreatorsWordsMobile';
 
 const Container = styled.div`
   width: 100%;
@@ -19,6 +20,7 @@ function InfoMobile() {
       <InfoBenefit />
       <InfoStatisticAndReview />
       <InfoOurService />
+      <InfoCreatorsWordsMobile />
     </Container>
   );
 }
