a1c4fc8463336bc320cf32aebc802113bfb0b86e
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-24 13:20:18 +0900

    Edit apply background

--- src/pages/user/info/InfoMobile.tsx
+++ src/pages/user/info/InfoMobile.tsx
@@ -7,9 +7,14 @@ import InfoStatisticAndReview from '@components/user/info/info-statistic-and-rev
 import InfoOurService from '@components/user/info/info-our-service/InfoOurService';
 import InfoCreatorsWordsMobile from '@components/user/info/info-creators-word/InfoCreatorsWordsMobile';
 import InfoFooter from '@components/user/info/info-footer/InfoFooter';
+import infoBackground from '@resources/image/infoBackground.png';
 
 const Container = styled.div`
   width: 100%;
+  background-image: url(${infoBackground});
+  background-repeat: repeat-y;
+  background-size: 100% 100%;
+  background-position: center;
   box-sizing: border-box;
   ${colFlex()}
 `;
