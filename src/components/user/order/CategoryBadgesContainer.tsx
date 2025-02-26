0ee6531cdfcbdab0c99b4257359d98466c4f572a
Rune <sungjong5k@naver.com>
2025-02-27 02:23:20 +0900

    Feat: Add scroll tracking code for selecting viewing category

--- src/components/user/order/CategoryBadgesContainer.tsx
+++ src/components/user/order/CategoryBadgesContainer.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
-import AppBadge from '@components/common/badge/AppBadge';
 import { Product, ProductCategory } from '@@types/index';
 import _ from 'lodash';
-import { rowFlex } from '@styles/flexStyles';
+import { colFlex, rowFlex } from '@styles/flexStyles';
 import { Color } from '@resources/colors';
 
 interface CategoryBadgesContainerProps {
@@ -15,7 +14,7 @@ interface CategoryBadgesContainerProps {
 const Container = styled.div`
   width: 100%;
   height: 50px;
-  padding-left: 35px;
+  padding: 0 10px;
   box-sizing: border-box;
   gap: 8px;
   overflow: scroll;
@@ -27,36 +26,83 @@ const Container = styled.div`
   }
 `;
 
+const CategoryLabel = styled.div<{ isSelected?: boolean }>`
+  width: auto;
+  padding: 0 10px;
+  border-bottom: ${({ isSelected }) => (isSelected ? '3px solid black' : 'none')};
+  ${colFlex({ justify: 'center' })}
+`;
+
 function CategoryBadgesContainer({ productCategories, productsByCategory, categoryRefs }: CategoryBadgesContainerProps) {
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+
   const scrollToCategory = (categoryId: string) => {
     const categoryElement = categoryRefs.current[categoryId];
 
-    if (categoryElement) {
-      const elementPosition = categoryElement.getBoundingClientRect().top;
-      const headerHeight = 110;
-      const offsetPosition = elementPosition + window.scrollY - headerHeight;
-
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: 'smooth',
-      });
+    if (!categoryElement) {
+      return;
     }
+
+    const elementPosition = categoryElement.getBoundingClientRect().top;
+    const headerHeight = 110;
+    const offsetPosition = elementPosition + window.scrollY - headerHeight;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
   };
 
+  const updateActiveCategory = () => {
+    const headerHeight = 110;
+    const scrollPosition = window.scrollY + headerHeight;
+
+    let newActiveCategory: string | null = null;
+
+    Object.entries(categoryRefs.current).forEach(([key, element]) => {
+      if (!element) {
+        return;
+      }
+
+      const elementTop = element.offsetTop;
+      const elementHeight = element.offsetHeight;
+      const isInSectionBounds = scrollPosition >= elementTop && scrollPosition < elementTop + elementHeight;
+
+      if (isInSectionBounds) {
+        newActiveCategory = key;
+        return;
+      }
+    });
+
+    setActiveCategory(newActiveCategory);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateActiveCategory);
+
+    return () => {
+      window.removeEventListener('scroll', updateActiveCategory);
+    };
+  }, []);
+
   return (
-    <Container className={'category-badges-container'}>
+    <Container className="category-badges-container">
       {productCategories.map(
         (category) =>
           productsByCategory[category.id] && (
-            <AppBadge onClick={() => scrollToCategory(String(category.id))} key={`category_${category.id}`}>
+            <CategoryLabel
+              onClick={() => scrollToCategory(String(category.id))}
+              key={`category_${category.id}`}
+              isSelected={activeCategory === String(category.id)}
+            >
               {category.name}
-            </AppBadge>
+            </CategoryLabel>
           ),
       )}
       {productsByCategory.undefined && (
-        <AppBadge onClick={() => scrollToCategory('.')} key={`category_null`}>
+        <CategoryLabel onClick={() => scrollToCategory('.')} key={`category_null`} isSelected={activeCategory === '.'}>
           기본 메뉴
-        </AppBadge>
+        </CategoryLabel>
       )}
     </Container>
   );
