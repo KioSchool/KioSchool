7927d1a47eb6031ad60cba6db5c1380e80b237e3
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-26 14:03:37 +0900

    Edit sungjong creator info

--- src/resources/data/infoData.ts
+++ src/resources/data/infoData.ts
@@ -12,10 +12,9 @@ export const infoCreatorData = [
   },
   {
     imageUrl: sungjong,
-    description:
-      '키오스쿨은 학교 주점을 운영하면서 느꼈던 관리의 어려움을 해결하기 위해 시작되었습니다. 이전 학생회 분들이 만든 비트페이에서 영감을 받아, 모든 주점 운영자들이 쉽게 사용할 수 있는 프로그램을 만들고자 했습니다. 주점은 일회성 이벤트가 많기 때문에, 간편하고 편리한 시스템을 통해 운영의 부담을 덜어주는 것을 최우선으로 생각했습니다.',
+    description: '키오스쿨의 직관적이고 세련된 UI가 학교 주점 운영의 복잡함을 단순화하는 핵심 열쇠라고 믿습니다!',
     creatorName: 'SUNGJONG LEE',
-    creatorDescription: '키오스쿨 ???',
+    creatorDescription: '키오스쿨 본부장',
   },
   {
     imageUrl: ahyoung,
