1da2a0930c1e3e77d8dbbf08407d5b575e86ddb3
Rune <sungjong5k@naver.com>
2025-02-28 16:35:06 +0900

    Style: Add 20px padding to NoticeContent

--- src/components/user/order/WorkspaceNotice.tsx
+++ src/components/user/order/WorkspaceNotice.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Color } from '@resources/colors';
+import { colFlex } from '@styles/flexStyles';
 
 const Container = styled.div`
   width: 100%;
   height: 70px;
+  ${colFlex({ align: 'center' })}
+`;
+
+const NoticeContent = styled.div`
+  width: calc(100% - 40px);
+  height: 100%;
   background: ${Color.LIGHT_GREY};
 `;
 
 function WorkspaceNotice() {
-  return <Container>WorkspaceNotice</Container>;
+  return (
+    <Container>
+      <NoticeContent>WorkspaceNotice</NoticeContent>
+    </Container>
+  );
 }
 
 export default WorkspaceNotice;
