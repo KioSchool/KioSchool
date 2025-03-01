66d218dc9332d3d3d54113ff3e3a84bd5b45964f
Rune <sungjong5k@naver.com>
2025-03-02 02:00:17 +0900

    Feat: Add ChevronDownSvg and apply icon styles

--- src/components/user/order/WorkspaceNotice.tsx
+++ src/components/user/order/WorkspaceNotice.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Color } from '@resources/colors';
-import { colFlex } from '@styles/flexStyles';
+import { colFlex, rowFlex } from '@styles/flexStyles';
+import ChevronDownSvg from '@resources/svg/ChevronDownSvg';
+import { expandButtonStyle } from '@styles/buttonStyles';
 
 const Container = styled.div`
   width: 100%;
-  height: 70px;
+  height: 60px;
+  margin: 15px 0;
   ${colFlex({ align: 'center' })}
 `;
 
 const NoticeContent = styled.div`
+  border-radius: 8px;
   width: calc(100% - 40px);
   height: 100%;
   background: ${Color.LIGHT_GREY};
+  ${rowFlex({ justify: 'space-between', align: 'center' })}
+`;
+
+const OpenIcon = styled(ChevronDownSvg)`
+  ${expandButtonStyle}
+  padding-right: 5px;
+  width: 25px;
+  height: 25px;
 `;
 
 function WorkspaceNotice() {
   return (
     <Container>
-      <NoticeContent>WorkspaceNotice</NoticeContent>
+      <NoticeContent>
+        WorkspaceNotice
+        <OpenIcon />
+      </NoticeContent>
     </Container>
   );
 }
