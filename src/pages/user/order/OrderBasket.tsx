ce9fe0c046f5346c7a870e7f39fd1de897113ce4
Rune <sungjong5k@naver.com>
2025-03-03 14:17:15 +0900

    Fix: Adjust layout to show header when component mount

--- src/pages/user/order/OrderBasket.tsx
+++ src/pages/user/order/OrderBasket.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -14,7 +13,7 @@ import AppLabel from '@components/common/label/AppLabel';
 const Container = styled.div`
   width: 100vw;
   min-height: 100vh;
-  padding: 0 0 80px;
+  padding-top: 45px;
   box-sizing: border-box;
 `;
 
@@ -22,7 +21,6 @@ const SubContainer = styled.div`
   box-sizing: border-box;
   width: 100%;
   padding: 0 20px;
-  margin-top: 45px;
   gap: 20px;
   border-top: 10px solid ${Color.LIGHT_GREY};
   padding-top: 12px;
@@ -66,15 +64,9 @@ function OrderBasket() {
   const workspaceId = searchParams.get('workspaceId');
   const tableNo = searchParams.get('tableNo');
 
-  const basketRef = React.useRef<HTMLDivElement>(null);
-
   if (orderBasket.length == 0) {
     navigate(-1);
   }
-
-  useEffect(() => {
-    basketRef.current?.scroll({ top: 0, behavior: 'smooth' });
-  }, []);
 
   const flushOrderBasket = () => {
     setOrderBasket([]);
@@ -90,7 +82,7 @@ function OrderBasket() {
           </AppLabel>
           <Button onClick={flushOrderBasket}>전체 삭제 </Button>
         </Header>
-        <OrderBasketContainer ref={basketRef} className={'order-basket-content'}>
+        <OrderBasketContainer className={'order-basket-content'}>
           {orderBasket.map((basket) => {
             const product = productsMap[basket.productId];
             return <ProductCounterBadge product={product} key={product.id} />;
