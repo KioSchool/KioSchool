cc72bc3acf7b182401d63f72b5b64416fc723a8f
Rune <sungjong5k@naver.com>
2025-03-08 16:52:28 +0900

    Feat: Add AccountInfo

--- src/components/admin/order/OrderPayAccountInfo.tsx
+++ src/components/admin/order/OrderPayAccountInfo.tsx
@@ -7,24 +7,26 @@ import React from 'react';
 const Container = styled.div`
   width: 100%;
   height: 140px;
+  gap: 5px;
   ${colFlex({ justify: 'center', align: 'center' })}
 `;
 
 const TitleContainer = styled.div`
-  width: 80%;
+  width: 87%;
   height: 30px;
   ${rowFlex({ justify: 'space-between', align: 'center' })}
 `;
 
 const AccountInfo = styled.div`
   width: 100%;
+  height: 100px;
   font-size: 13px;
   font-weight: 500;
   color: #898989;
   padding: 0 40px;
-  text-align: center;
   box-sizing: border-box;
   background: ${Color.LIGHT_GREY};
+  ${colFlex({ justify: 'center', align: 'center' })}
 `;
 
 const CopyButton = styled.button`
@@ -37,6 +39,30 @@ const CopyButton = styled.button`
   ${rowFlex({ justify: 'center', align: 'center' })}
 `;
 
+const InfoRow = styled.div`
+  width: 100%;
+  gap: 5px;
+  ${rowFlex({ justify: 'center', align: 'center' })}
+`;
+
+const Key = styled.div`
+  width: 90px;
+  ${rowFlex({ justify: 'end', align: 'center' })}
+`;
+
+const Value = styled.div`
+  width: 250px;
+  ${rowFlex({ justify: 'start', align: 'center' })}
+`;
+
+const Divider = styled.span``;
+
+const DummyAccountInfo = {
+  bankName: '국민은행',
+  accountNumber: '620002-04-112345',
+  depositor: '김지인',
+};
+
 function OrderPayAccountInfo() {
   return (
     <Container>
@@ -44,7 +70,23 @@ function OrderPayAccountInfo() {
         <AppLabel size={15}>계좌 정보</AppLabel>
         <CopyButton>계좌 복사하기</CopyButton>
       </TitleContainer>
-      <AccountInfo>OrderPayAccountInfo</AccountInfo>
+      <AccountInfo>
+        <InfoRow>
+          <Key>은행명</Key>
+          <Divider>|</Divider>
+          <Value>{DummyAccountInfo.bankName}</Value>
+        </InfoRow>
+        <InfoRow>
+          <Key>계좌번호</Key>
+          <Divider>|</Divider>
+          <Value>{DummyAccountInfo.accountNumber}</Value>
+        </InfoRow>
+        <InfoRow>
+          <Key>예금주</Key>
+          <Divider>|</Divider>
+          <Value>{DummyAccountInfo.depositor}</Value>
+        </InfoRow>
+      </AccountInfo>
     </Container>
   );
 }
