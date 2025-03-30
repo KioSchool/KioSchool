811a853c514b3a4a8ef9126a6e386e17dedb90bf
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-30 12:59:19 +0900

    Apply AppContainer to AdminAccount

--- src/pages/admin/AdminAccount.tsx
+++ src/pages/admin/AdminAccount.tsx
@@ -1,14 +1,35 @@
 import AccountInfo from '@components/admin/account/AccountInfo';
 import RegisterAccount from '@components/admin/account/RegisterAccount';
 import RegisterTossAccount from '@components/admin/account/RegisterTossAccount';
+import AppContainer from '@components/common/container/AppContainer';
+import { colFlex } from '@styles/flexStyles';
+import { useNavigate, useParams } from 'react-router-dom';
+import useAdminWorkspace from '@hooks/admin/useAdminWorkspace';
+import { useRecoilValue } from 'recoil';
+import { adminWorkspaceAtom } from '@recoils/atoms';
+import { useEffect } from 'react';
 
 function AdminAccount() {
+  const { workspaceId } = useParams<{ workspaceId: string }>();
+  const { fetchWorkspace } = useAdminWorkspace();
+  const navigate = useNavigate();
+  const workspace = useRecoilValue(adminWorkspaceAtom);
+
+  useEffect(() => {
+    fetchWorkspace(workspaceId);
+  }, []);
+
   return (
-    <div>
-      <AccountInfo />
-      <RegisterAccount />
-      <RegisterTossAccount />
-    </div>
+    <AppContainer
+      useFlex={colFlex({ justify: 'center' })}
+      titleNavBarProps={{ title: workspace.name, subTitle: workspace.description, onLeftArrowClick: () => navigate('/admin/my-info') }}
+    >
+      <>
+        <AccountInfo />
+        <RegisterAccount />
+        <RegisterTossAccount />
+      </>
+    </AppContainer>
   );
 }
 
