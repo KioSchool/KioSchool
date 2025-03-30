998bb3343d9d931f700f3e92c9ac7ac498834bef
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-30 18:49:49 +0900

    Create tossAcountInfoData.ts

---
+++ src/resources/data/tossAcountInfoData.ts
@@ -0,0 +1,45 @@
+export const beforeAccountInfoRegisteredDescription = [
+  {
+    text: '계좌 등록',
+    useHighlight: true,
+  },
+  { text: ' 후 토스 QR을 등록해주세요.', useHighlight: false },
+];
+
+export const afterAccountInfoRegisteredDescription = [
+  {
+    text: '토스 QR을',
+    useHighlight: true,
+  },
+  {
+    text: ' 등록하면 \n',
+    useHighlight: false,
+  },
+  {
+    text: '계좌번호를 복사해 직접 송금할 필요 없이, \n',
+    useHighlight: false,
+  },
+  {
+    text: '바로 토스로 연결',
+    useHighlight: true,
+  },
+  { text: '돼 간편히 결제할 수 있습니다.', useHighlight: false },
+  { text: '\n\n', useHighlight: false },
+  { text: '1. ', useHighlight: false },
+  { text: '토스', useHighlight: true },
+  { text: ' 앱 실행', useHighlight: false },
+  { text: '\n\n', useHighlight: false },
+  { text: '2. 하단 ', useHighlight: false },
+  { text: '전체메뉴', useHighlight: true },
+  { text: ' 선택', useHighlight: false },
+  { text: '\n\n', useHighlight: false },
+  { text: '3. ', useHighlight: false },
+  { text: '사진으로 송금', useHighlight: true },
+  { text: ' 메뉴 검색', useHighlight: false },
+  { text: '\n\n', useHighlight: false },
+  { text: '4. 계좌 선택 후 ', useHighlight: false },
+  { text: '받을 금액 설정 x', useHighlight: true },
+  { text: '\n\n', useHighlight: false },
+  { text: '5. ', useHighlight: false },
+  { text: 'QR 코드 발급', useHighlight: true },
+];
