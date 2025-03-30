60994822dde16c9914653397745b440dcfa3b944
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-30 13:37:42 +0900

    Apply basic AdminAccount layout

--- src/pages/admin/AdminAccount.tsx
+++ src/pages/admin/AdminAccount.tsx
@@ -2,33 +2,47 @@ import AccountInfo from '@components/admin/account/AccountInfo';
 import RegisterAccount from '@components/admin/account/RegisterAccount';
 import RegisterTossAccount from '@components/admin/account/RegisterTossAccount';
 import AppContainer from '@components/common/container/AppContainer';
-import { colFlex } from '@styles/flexStyles';
-import { useNavigate, useParams } from 'react-router-dom';
-import useAdminWorkspace from '@hooks/admin/useAdminWorkspace';
+import { colFlex, rowFlex } from '@styles/flexStyles';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { adminWorkspaceAtom } from '@recoils/atoms';
+import { adminUserAtom } from '@recoils/atoms';
 import { useEffect } from 'react';
+import styled from '@emotion/styled';
+import HorizontalDivider from '@components/common/divider/HorizontalDivider';
+import useAdminUser from '@hooks/admin/useAdminUser';
+
+const AccountContainer = styled.div`
+  width: 100%;
+  gap: 40px;
+  ${colFlex({ align: 'center' })};
+`;
+
+const RegisterContainer = styled.div`
+  width: 100%;
+  ${rowFlex({ align: 'center' })};
+`;
 
 function AdminAccount() {
-  const { workspaceId } = useParams<{ workspaceId: string }>();
-  const { fetchWorkspace } = useAdminWorkspace();
+  const { fetchAdminUser } = useAdminUser();
   const navigate = useNavigate();
-  const workspace = useRecoilValue(adminWorkspaceAtom);
-
+  const user = useRecoilValue(adminUserAtom);
   useEffect(() => {
-    fetchWorkspace(workspaceId);
+    fetchAdminUser();
   }, []);
 
   return (
     <AppContainer
       useFlex={colFlex({ justify: 'center' })}
-      titleNavBarProps={{ title: workspace.name, subTitle: workspace.description, onLeftArrowClick: () => navigate('/admin/my-info') }}
+      titleNavBarProps={{ title: `${user.name}님의 마이페이지`, subTitle: '계좌관리', onLeftArrowClick: () => navigate('/admin/my-info') }}
     >
-      <>
+      <AccountContainer>
         <AccountInfo />
-        <RegisterAccount />
-        <RegisterTossAccount />
-      </>
+        <HorizontalDivider />
+        <RegisterContainer>
+          <RegisterAccount />
+          <RegisterTossAccount />
+        </RegisterContainer>
+      </AccountContainer>
     </AppContainer>
   );
 }
