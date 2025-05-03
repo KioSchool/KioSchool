0f56d88e32f87e7760bce1dafc3e1a1a2f67dc43
Rune <sungjong5k@naver.com>
2025-05-04 03:46:58 +0900

    Feat: Add QR code download function

--- src/components/common/container/PreviewContainer.tsx
+++ src/components/common/container/PreviewContainer.tsx
@@ -3,9 +3,12 @@ import styled from '@emotion/styled';
 import { colFlex, rowFlex } from '@styles/flexStyles';
 import { Color } from '@resources/colors';
 import CellularConnectionSvg from '@resources/svg/preview/CellularConnectionSvg';
-import { RiBatteryFill, RiExternalLinkLine, RiWifiFill } from '@remixicon/react';
+import { RiBatteryFill, RiExternalLinkLine, RiWifiFill, RiQrCodeLine } from '@remixicon/react';
 import AppLabel from '@components/common/label/AppLabel';
 import { useParams } from 'react-router-dom';
+import { QRCodeCanvas } from 'qrcode.react';
+import * as ReactDOM from 'react-dom/client';
+import { flushSync } from 'react-dom';
 
 const Container = styled.div<{ width: number; height: number }>`
   ${colFlex({ justify: 'center', align: 'center' })}
@@ -52,8 +55,13 @@ const WifiIcon = styled(RiWifiFill)`
 `;
 
 const LinkIcon = styled(RiExternalLinkLine)`
-  width: 12px;
-  height: 12px;
+  width: 15px;
+  height: 15px;
+`;
+
+const QRCodeIcon = styled(RiQrCodeLine)`
+  width: 15px;
+  height: 15px;
 `;
 
 const FooterContainer = styled.div`
@@ -62,17 +70,23 @@ const FooterContainer = styled.div`
   height: 30px;
   font-size: 15px;
   color: ${Color.GREY};
-  gap: 10px;
-  cursor: pointer;
-  &:hover {
-    color: ${Color.KIO_ORANGE};
-  }
+  gap: 15px;
 `;
 
 const PreviewContent = styled.iframe`
   width: 100%;
   height: 100%;
   border: none;
+`;
+
+const LinkContainer = styled.div`
+  max-width: 350px;
+  ${rowFlex({ justify: 'center', align: 'center' })}
+  cursor: pointer;
+  gap: 3px;
+  &:hover {
+    color: ${Color.KIO_ORANGE};
+  }
 `;
 
 interface PreviewContainerProps {
@@ -87,6 +101,54 @@ function PreviewContainer({ width = 360, height = 700 }: PreviewContainerProps) 
 
   const onClickPreviewLink = () => {
     window.open(previewUrl, '_blank');
+  };
+
+  const onClickDownloadQRCode = () => {
+    const imageDiv = document.createElement('div');
+    Object.assign(imageDiv.style, {
+      position: 'absolute',
+      left: '-9999px',
+      background: 'white',
+      padding: '10px',
+      borderRadius: '10px',
+    });
+
+    document.body.appendChild(imageDiv);
+
+    const root = ReactDOM.createRoot(imageDiv);
+
+    flushSync(() => {
+      root.render(<QRCodeCanvas value={previewUrl} size={200} level="H" bgColor="#FFFFFF" fgColor="#000000" />);
+    });
+
+    try {
+      const canvasElement = imageDiv.querySelector('canvas');
+
+      if (!canvasElement) {
+        throw new Error('QR 코드 캔버스 요소를 찾을 수 없습니다.');
+      }
+
+      const dataUrl = canvasElement.toDataURL();
+
+      const link = document.createElement('a');
+      link.download = '미리보기_QR코드.png';
+      link.href = dataUrl;
+      link.click();
+
+      root.unmount();
+      document.body.removeChild(imageDiv);
+    } catch (error) {
+      console.error('QR 코드 처리 중 오류가 발생했습니다:', error);
+      alert('QR 코드 다운로드 중 오류가 발생했습니다!');
+
+      if (root) {
+        root.unmount();
+      }
+
+      if (document.body.contains(imageDiv)) {
+        document.body.removeChild(imageDiv);
+      }
+    }
   };
 
   return (
@@ -106,9 +168,15 @@ function PreviewContainer({ width = 360, height = 700 }: PreviewContainerProps) 
         </IndicatorContainer>
         <PreviewContent src={previewUrl} />
       </DeviceContainer>
-      <FooterContainer onClick={onClickPreviewLink}>
-        <LinkIcon />
-        주문화면 미리보기
+      <FooterContainer>
+        <LinkContainer onClick={onClickPreviewLink}>
+          <LinkIcon />
+          주문화면 미리보기
+        </LinkContainer>
+        <LinkContainer onClick={onClickDownloadQRCode}>
+          <QRCodeIcon />
+          미리보기 QR 다운로드
+        </LinkContainer>
       </FooterContainer>
     </Container>
   );
