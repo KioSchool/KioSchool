867d74aa354bfa8647f0cea967e889266eeb52dd
Rune <sungjong5k@naver.com>
2025-05-05 03:18:55 +0900

    Feat: Apply new design workspace container

--- src/components/admin/workspace/WorkspaceContent.tsx
+++ src/components/admin/workspace/WorkspaceContent.tsx
@@ -3,17 +3,19 @@ import styled from '@emotion/styled';
 import useAdminUser from '@hooks/admin/useAdminUser';
 import useCustomNavigate from '@hooks/useCustomNavigate';
 import useConfirm from '@hooks/useConfirm';
-import { RiDeleteBinLine } from '@remixicon/react';
+import { RiDeleteBinFill } from '@remixicon/react';
 import React from 'react';
 import { colFlex, rowFlex } from '@styles/flexStyles';
 import { Color } from '@resources/colors';
 import { expandButtonStyle } from '@styles/buttonStyles';
 
 const WorkspaceContainer = styled.div`
+  box-sizing: border-box;
   cursor: pointer;
   width: 321px;
   height: 332px;
   border-radius: 25px;
+  padding: 20px;
   background: ${Color.KIO_ORANGE};
   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
   ${colFlex({ justify: 'space-between', align: 'flex-start' })}
@@ -30,34 +32,21 @@ const WorkspaceContainer = styled.div`
 `;
 
 const MenuTitle = styled.div`
-  padding: 0 0 25px 22px;
-  width: 164px;
-  height: 27px;
-  flex-shrink: 0;
   color: #fff;
-  font-size: 24px;
-  font-weight: 300;
+  font-size: 20px;
+  font-weight: 500;
   ${rowFlex()}
 `;
 
-const DeleteContainer = styled.div`
-  position: relative;
-  padding: 12px 15px 0 0;
-`;
-
-const DeleteButton = styled(RiDeleteBinLine)`
-  position: absolute;
+const DeleteButton = styled(RiDeleteBinFill)`
   color: ${Color.WHITE};
-  right: 15px;
-  top: 12px;
-  width: 30px;
-  height: 30px;
   ${expandButtonStyle}
 `;
 
 const TitleContainer = styled.div`
   width: 100%;
   flex-wrap: wrap;
+  word-break: keep-all;
   ${colFlex()}
 `;
 
@@ -70,22 +59,16 @@ const MainTitleContainer = styled.div`
 const SubTitleContainer = styled.div``;
 
 const Description = styled.div`
-  padding: 26px 10px 0 23px;
-  flex-wrap: wrap;
-  width: 198px;
-  color: #fff;
-  font-size: 32px;
-  font-weight: 100;
+  width: calc(100% - 30px);
+  color: ${Color.WHITE};
+  font-size: 20px;
+  font-weight: 500;
   ${rowFlex()}
 `;
 
 const Title = styled.div`
-  padding: 5px 10px 22px 22px;
-  flex-wrap: wrap;
-  width: 69%;
-  height: 50px;
-  color: #fff;
-  font-size: 40px;
+  color: ${Color.WHITE};
+  font-size: 25px;
   font-weight: 700;
   ${rowFlex()}
 `;
@@ -123,13 +106,11 @@ function WorkspaceContent({ workspaces }: Props) {
           <TitleContainer className={'title-container'}>
             <MainTitleContainer className={'main-title-container'}>
               <Description className={'description'}>{it.description}</Description>
-              <DeleteContainer className={'delete-container'}>
-                <DeleteButton
-                  onClick={(e: React.FormEvent) => {
-                    leaveHandler(e, it.id);
-                  }}
-                />
-              </DeleteContainer>
+              <DeleteButton
+                onClick={(e: React.FormEvent) => {
+                  leaveHandler(e, it.id);
+                }}
+              />
             </MainTitleContainer>
 
             <SubTitleContainer className={'sub-title-container'}>
