67649f4210044a62587ee6c63dfae4e6a42223be
Rune <sungjong5k@naver.com>
2025-05-06 02:50:09 +0900

    Feat: Edit Register component styles and logic

--- src/pages/user/Register.tsx
+++ src/pages/user/Register.tsx
@@ -1,59 +1,39 @@
 import React, { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
-import AppLabel from '@components/common/label/AppLabel';
-import AppInputWithLabel from '@components/common/input/AppInputWithLabel';
-import AppButton from '@components/common/button/AppButton';
-import AppFooter from '@components/common/footer/AppFooter';
-import { colFlex, rowFlex } from '@styles/flexStyles';
+import { colFlex } from '@styles/flexStyles';
 import useRegister from '@hooks/user/useRegister';
 import AppContainer from '@components/common/container/AppContainer';
-
-const SubContainer = styled.div`
-  width: 500px;
-  flex-basis: 0;
-  flex-wrap: wrap;
-  height: inherit;
-  ${colFlex({ justify: 'center', align: 'flex-start' })}
-`;
+import NewAppInput from '@components/common/input/NewAppInput';
+import NewRoundedButton from '@components/common/button/NewRoundedButton';
+import { Color } from '@resources/colors';
+import LinkLabel from '@components/common/label/LinkLabel';
 
 const FormContainer = styled.form`
   flex-wrap: wrap;
   gap: 14px;
   ${colFlex({ justify: 'center', align: 'center' })}
 `;
-const IdContainer = styled.div`
-  width: 100%;
-  flex-wrap: wrap;
-  ${rowFlex({ justify: 'space-between', align: 'flex-end' })}
-`;
+
+const PasswordContainer = styled.div``;
 
 const EmailContainer = styled.div`
   width: 100%;
   flex-wrap: wrap;
   gap: 12px;
-  ${rowFlex({ justify: 'space-between', align: 'flex-end' })}
+  ${colFlex({ justify: 'center', align: 'center' })}
 `;
 
-const CodeContainer = styled.div`
+const ErrorMessageContainer = styled.div`
   width: 100%;
-  flex-wrap: wrap;
-  ${rowFlex({ justify: 'space-between', align: 'flex-end' })}
+  height: 20px;
+  ${colFlex({ justify: 'center', align: 'center' })};
 `;
-
-const LoginLabelContainer = styled.div`
-  width: 100%;
-  padding: 20px 0 0;
-  ${rowFlex({ justify: 'center', align: 'center' })}
-`;
-
-const CodeReSendStyle = { width: '90px' };
-
-const RegisterStyle = { marginTop: '25px' };
 
 const ErrorMessage = styled.div`
-  padding: 0 0 5px;
-  color: #ff0000;
+  font-size: 13px;
+  font-weight: 500;
+  color: ${Color.KIO_ORANGE};
 `;
 
 function Register() {
@@ -68,21 +48,53 @@ function Register() {
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [isAbleId, setIsAbleId] = useState<boolean>(false);
 
+  const [isAblePassword, setIsAblePassword] = useState<boolean>(false);
+
   const [isCodeSent, setIsCodeSent] = useState<boolean>(false);
   const [isVerified, setIsVerified] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
-  const isSamePassword = userPasswordInputRef.current?.value === checkUserPasswordInput;
-  const showCheckPasswordLabel = () => {
-    if (!checkUserPasswordInput) return undefined;
-    return isSamePassword ? '비밀번호가 동일합니다!' : '비밀번호가 서로 다릅니다.';
+  const validatePassword = (checkInput: string) => {
+    const password = userPasswordInputRef.current?.value;
+
+    if (password?.includes(' ')) {
+      setErrorMessage('비밀번호에 공백이 포함되어 있습니다.');
+      return;
+    }
+
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[a-zA-Z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,20}$/;
+    if (password && !passwordRegex.test(password)) {
+      setErrorMessage('비밀번호는 영문, 숫자, 특수문자 조합 8~20자로 입력해주세요.');
+      return;
+    }
+
+    const isSamePassword = password === checkInput;
+    if (!isSamePassword) {
+      setErrorMessage('비밀번호가 서로 다릅니다.');
+      return;
+    }
+
+    setErrorMessage('');
+    setIsAblePassword(true);
   };
 
-  const checkDuplicate = async () => {
+  const validateId = async () => {
     const userId = userIdInputRef.current?.value;
+
+    if (userId?.includes(' ')) {
+      setErrorMessage('아이디에 공백이 포함되어 있습니다.');
+      return;
+    }
+
     if (!userId) {
       setErrorMessage('아이디가 입력되지 않았습니다.');
+      return;
+    }
+
+    const idRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{4,20}$/;
+    if (!idRegex.test(userId)) {
+      setErrorMessage('아이디는 영문, 숫자 조합 4~20자로 입력해주세요.');
       return;
     }
 
@@ -99,37 +111,18 @@ function Register() {
 
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!isAbleId) {
-      setErrorMessage('아이디 중복체크를 해주세요.');
+    const userName = userNameInputRef.current?.value;
+    const userId = userIdInputRef.current?.value;
+    const password = userPasswordInputRef.current?.value;
+    const userEmail = userEmailInputRef.current?.value;
+
+    if (!isAbleId || !userName || !userId || !password || !userEmail) {
+      setErrorMessage('모두 올바르게 입력했는지 확인해주세요!');
       return;
     }
 
     if (!isVerified) {
-      setErrorMessage('이메일 인증을 해주세요.');
-      return;
-    }
-
-    const userName = userNameInputRef.current?.value;
-    if (!userName) {
-      setErrorMessage('이름을 입력해주세요.');
-      return;
-    }
-
-    const userId = userIdInputRef.current?.value;
-    if (!userId) {
-      setErrorMessage('아이디를 입력해주세요.');
-      return;
-    }
-
-    const password = userPasswordInputRef.current?.value;
-    if (!password) {
-      setErrorMessage('비밀번호를 입력해주세요.');
-      return;
-    }
-
-    const userEmail = userEmailInputRef.current?.value;
-    if (!userEmail) {
-      setErrorMessage('이메일을 입력해주세요.');
+      setErrorMessage('이메일 인증을 해주세요!');
       return;
     }
 
@@ -146,10 +139,27 @@ function Register() {
 
   const sendCode = async () => {
     setErrorMessage('');
+    setIsVerified(false);
+
     const userEmail = userEmailInputRef.current?.value;
 
     if (!userEmail) {
-      alert('이메일을 입력해주세요');
+      setErrorMessage('이메일을 입력해주세요');
+      return;
+    }
+
+    if (userNameInputRef.current?.value === '') {
+      setErrorMessage('이름을 입력해주세요');
+      return;
+    }
+
+    if (!isAbleId) {
+      setErrorMessage('아이디 중복체크를 해주세요');
+      return;
+    }
+
+    if (!isAblePassword) {
+      setErrorMessage('비밀번호를 확인해주세요');
       return;
     }
 
@@ -163,16 +173,12 @@ function Register() {
     setIsCodeSent(true);
   };
 
-  const navigateToEmailDomain = () => {
-    navigate('/email-domains');
-  };
-
   const checkCode = async () => {
     const userEmail = userEmailInputRef.current?.value;
     const inputCode = userCodeInputRef.current?.value;
 
     if (!userEmail || !inputCode) {
-      alert('이메일 또는 입력코드가 올바른지 확인해주세요');
+      setErrorMessage('이메일 또는 입력코드가 올바른지 확인해주세요');
       return;
     }
 
@@ -181,108 +187,85 @@ function Register() {
     setIsVerified(response.isVerify);
     if (!response.isVerify) {
       setErrorMessage(response.errorMessage);
+    } else {
+      setErrorMessage('');
     }
   };
 
   return (
-    <AppContainer useFlex={rowFlex({ justify: 'center' })}>
-      <SubContainer className={'register-sub-container'}>
-        <AppLabel size={'large'} style={{ padding: '0 0 30px' }}>
-          회원가입
-        </AppLabel>
-
-        {errorMessage && <ErrorMessage className="error-message">{errorMessage}</ErrorMessage>}
+    <AppContainer useFlex={colFlex({ justify: 'center', align: 'center' })} titleNavBarProps={{ title: '회원가입', useBackIcon: false }}>
+      <>
         <FormContainer id={'form'} onSubmit={submitHandler}>
-          <AppInputWithLabel titleLabel={'이름'} type={'text'} id={'name'} ref={userNameInputRef} placeholder="이름을 입력해주세요" required />
-          <IdContainer className={'id-container'}>
-            <AppInputWithLabel
-              style={{ width: '330px' }}
-              titleLabel={'아이디'}
-              messageLabel={'영문, 숫자 조합 4~20자'}
-              type={'text'}
-              id={'userId'}
-              ref={userIdInputRef}
-              onChange={() => {
-                setIsAbleId(false);
-              }}
-              placeholder="아이디를 입력해주세요."
-              required
-            />
-            <AppButton size={'medium'} type={'button'} onClick={checkDuplicate} disabled={isAbleId}>
-              {isAbleId ? '사용 가능 ID' : 'ID 중복체크'}
-            </AppButton>
-          </IdContainer>
+          <NewAppInput label={'이름'} ref={userNameInputRef} placeholder={'이름을 입력해주세요.'} required />
 
-          <AppInputWithLabel
-            titleLabel={'비밀번호'}
-            messageLabel={'영문, 숫자, 특수문자 조합 8~20자'}
-            type={'password'}
-            id={'userPassword'}
+          <NewAppInput
+            id={'userId'}
+            ref={userIdInputRef}
             onChange={() => {
-              setCheckUserPasswordInput('');
+              setIsAbleId(false);
             }}
-            ref={userPasswordInputRef}
-            placeholder="비밀번호를 입력해주세요"
+            label={'아이디'}
+            placeholder={'영문, 숫자 조합 4~20자'}
+            buttonProps={{ text: isAbleId ? '사용 가능 ID' : 'ID 중복체크', disabled: isAbleId }}
+            enterHandler={validateId}
             required
           />
-          <AppInputWithLabel
-            titleLabel={'비밀번호 확인'}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setCheckUserPasswordInput(event.target.value)}
-            messageLabel={showCheckPasswordLabel()}
-            type={'password'}
-            value={checkUserPasswordInput}
-            placeholder="입력한 비밀번호를 똑같이 입력해주세요"
-            required
-          ></AppInputWithLabel>
+
+          <PasswordContainer>
+            <NewAppInput
+              id={'userPassword'}
+              ref={userPasswordInputRef}
+              label={'비밀번호'}
+              placeholder={'영문, 숫자, 특수문자 조합 8~20자'}
+              type={'password'}
+              onChange={() => {
+                setCheckUserPasswordInput('');
+              }}
+              required
+            />
+            <NewAppInput
+              id={'userPassword'}
+              placeholder={'입력한 비밀번호를 똑같이 입력해주세요.'}
+              type={'password'}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setCheckUserPasswordInput(event.target.value);
+                validatePassword(event.target.value);
+              }}
+              value={checkUserPasswordInput}
+              required
+            />
+          </PasswordContainer>
 
           <EmailContainer className={'email-container'}>
-            <AppInputWithLabel
-              style={{ width: '330px' }}
-              titleLabel={'이메일'}
-              messageLabel={'회원가입 가능한 이메일 확인하기'}
-              onMessageLabelClick={navigateToEmailDomain}
+            <NewAppInput
               type={'email'}
               id={'userEmail'}
               ref={userEmailInputRef}
-              placeholder="인증코드를 받을 이메일을 입력해주세요"
+              label={'이메일 주소'}
+              placeholder={'인증코드를 받을 이메일을 입력해주세요'}
+              linkProps={{ text: '회원가입 가능한 이메일 확인하기', url: '/email-domains' }}
+              buttonProps={{ text: isCodeSent ? '인증코드 재전송' : '인증코드 발송', type: 'button' }}
+              enterHandler={sendCode}
               required
             />
-            <AppButton type={'button'} onClick={sendCode}>
-              인증코드 전송
-            </AppButton>
+
             {isCodeSent && (
-              <CodeContainer className={'code-container'}>
-                <AppInputWithLabel
-                  style={{ width: '275px' }}
-                  titleLabel={'인증코드'}
-                  type={'text'}
-                  id={'code'}
-                  ref={userCodeInputRef}
-                  placeholder="받은 인증코드를 입력해주세요"
-                  required
-                />
-                <AppButton style={CodeReSendStyle} type={'button'} onClick={sendCode}>
-                  재전송
-                </AppButton>
-                <AppButton size={'small'} type={'button'} onClick={checkCode}>
-                  확인
-                </AppButton>
-              </CodeContainer>
+              <NewAppInput
+                id={'code'}
+                ref={userCodeInputRef}
+                label={'인증코드'}
+                placeholder={'인증코드를 입력해주세요.'}
+                buttonProps={{ text: !isVerified ? '인증하기' : '인증완료', type: 'button', disabled: isVerified }}
+                enterHandler={checkCode}
+                required
+              />
             )}
           </EmailContainer>
-
-          {isVerified && isCodeSent ? '인증 성공' : ''}
-          <AppButton size={'large'} style={RegisterStyle} type={'submit'}>
-            회원가입
-          </AppButton>
+          <ErrorMessageContainer>{errorMessage && <ErrorMessage className="error-message">{errorMessage}</ErrorMessage>}</ErrorMessageContainer>
+          <NewRoundedButton size={'sm'}>회원가입</NewRoundedButton>
         </FormContainer>
-        <LoginLabelContainer className={'login-label-container'}>
-          <AppLabel size={'small'}>
-            <Link to={'/login'}>로그인하기</Link>
-          </AppLabel>
-        </LoginLabelContainer>
-        <AppFooter />
-      </SubContainer>
+        <LinkLabel text={'로그인하기'} href={'/login'} style={{ fontSize: '13px', marginTop: '20px' }} />
+      </>
     </AppContainer>
   );
 }
