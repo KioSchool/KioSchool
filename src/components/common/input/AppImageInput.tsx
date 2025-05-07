3bdac3df37afc65aee2dbe0bcd3b23a277298b20
Rune <sungjong5k@naver.com>
2025-05-07 10:57:07 +0900

    Feat: Edit AppImageInput styles

--- src/components/common/input/AppImageInput.tsx
+++ src/components/common/input/AppImageInput.tsx
@@ -1,8 +1,55 @@
 import React, { ChangeEvent } from 'react';
 import styled from '@emotion/styled';
-import uploadPreview from '@resources/image/uploadPreview.png';
 import { colFlex, rowFlex } from '@styles/flexStyles';
-import { Color } from '@resources/colors';
+import NewRoundedButton from '../button/NewRoundedButton';
+import { RiCamera2Fill } from '@remixicon/react';
+
+const ImageInputContainer = styled.div`
+  gap: 8px;
+  ${colFlex({ align: 'center' })}
+`;
+
+const ImageLabelContainer = styled.div`
+  width: 100%;
+  font-size: 14px;
+  ${rowFlex({ justify: 'space-between', align: 'center' })}
+`;
+
+const StyledLabel = styled.div`
+  color: #5c5c5c;
+  font-size: 20px;
+  font-weight: 500;
+  padding-left: 10px;
+`;
+
+const UploadImageWrapper = styled.div`
+  width: 500px;
+  height: 500px;
+  border-radius: 30px;
+  border: 1px solid rgba(201, 201, 201, 0.5);
+  background: rgba(255, 255, 255, 0.1);
+  ${rowFlex({ justify: 'center', align: 'center' })};
+`;
+
+const UploadImage = styled(RiCamera2Fill)`
+  width: 50px;
+  height: 50px;
+  color: #5c5c5c;
+`;
+
+const ImageInput = styled.input`
+  display: none;
+`;
+
+const Image = styled.img<{ width?: number; height?: number }>`
+  width: ${(props) => (props.width ? `${props.width}px` : '500px')};
+  height: ${(props) => (props.height ? `${props.height}px` : '500px')};
+  border-radius: 30px;
+  border: 1px solid rgba(201, 201, 201, 0.5);
+  box-sizing: border-box;
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.25) inset;
+  object-fit: fill;
+`;
 
 interface ProductImageInputProps {
   title: string;
@@ -13,63 +60,35 @@ interface ProductImageInputProps {
   height?: number;
 }
 
-const ImageInputContainer = styled.div`
-  gap: 15px;
-  ${colFlex({ align: 'center' })}
-`;
-
-const ImageLabelContainer = styled.div`
-  width: 100%;
-  font-size: 14px;
-  ${rowFlex({ justify: 'space-between', align: 'center' })}
-`;
-
-const ImageInputButton = styled.label`
-  cursor: pointer;
-  padding: 5px 10px;
-  border-radius: 8px;
-  background-color: ${Color.KIO_ORANGE};
-  color: ${Color.WHITE};
-  &:hover {
-    background: #ff7b2b;
-  }
-`;
-
-const ImageInput = styled.input`
-  display: none;
-`;
-
-const Image = styled.img<{ width?: number; height?: number }>`
-  width: ${(props) => (props.width ? `${props.width}px` : '500px')};
-  height: ${(props) => (props.height ? `${props.height}px` : '400px')};
-  border: none;
-  border-radius: 15px;
-  box-sizing: border-box;
-  box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.25) inset;
-  object-fit: fill;
-`;
-
 function AppImageInput({ file, title, url, onImageChange, width, height }: ProductImageInputProps) {
   const getImageUrl = () => {
     if (file) {
       return URL.createObjectURL(file);
     }
+
     if (url) {
       return url;
     }
-    return uploadPreview;
+
+    return '';
   };
 
   return (
     <ImageInputContainer className={'app-image-input'}>
       <ImageLabelContainer className={'image-label-container'}>
-        <label>{title}</label>
-        <ImageInputButton htmlFor="img" className={'image-input-button'}>
+        <StyledLabel>{title}</StyledLabel>
+        <NewRoundedButton className={'image-input-button'} size={'xs'} onClick={() => document.getElementById('img')?.click()}>
           사진 업로드
-        </ImageInputButton>
+        </NewRoundedButton>
         <ImageInput type="file" id="img" accept="image/*" onChange={onImageChange} className={'image-input'} />
       </ImageLabelContainer>
-      <Image src={getImageUrl()} alt="" width={width} height={height} className={'image'} />
+      {file ? (
+        <Image src={getImageUrl()} alt="" width={width} height={height} className={'image'} />
+      ) : (
+        <UploadImageWrapper className={'upload-image-wrapper'}>
+          <UploadImage />
+        </UploadImageWrapper>
+      )}
     </ImageInputContainer>
   );
 }
