83bc29df93f770363f5d8d6fad95b57aff8f093e
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-05-11 02:59:32 +0900

    Update usePopup.tsx

--- src/hooks/usePopup.tsx
+++ src/hooks/usePopup.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Cookies } from 'react-cookie';
+import dayjs from 'dayjs';
+import useApi from '@hooks/useApi';
 
 function usePopup() {
+  const { adminApi } = useApi();
   const [isOpen, setIsOpen] = React.useState(false);
+
+  const sendPopupResult = (result: string) => {
+    adminApi.post('popup/result', { result });
+  };
+
+  const isValidPopup = (popupId: number, expireDate: Date) => {
+    const cookies = new Cookies();
+    const currentDate = new Date();
+
+    const cookie = cookies.get(`close_popup_${popupId}`);
+    if (cookie) {
+      return false;
+    }
+
+    return expireDate >= currentDate;
+  };
 
   const openPopup = () => {
     setIsOpen(true);
@@ -15,10 +35,20 @@ function usePopup() {
     document.body.style.overflow = 'auto';
   };
 
+  const closePopupForDay = (popupId: number) => {
+    const cookies = new Cookies();
+    const expireDate = dayjs().add(1, 'day').startOf('day').toDate();
+    cookies.set(`close_popup_${popupId}`, 'true', { path: '/', expires: expireDate });
+    closePopup();
+  };
+
   return {
     isOpen,
+    sendPopupResult,
     openPopup,
     closePopup,
+    closePopupForDay,
+    isValidPopup,
   };
 }
 
