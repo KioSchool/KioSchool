ea853d047173d6bc1732895b2fc2b1eaeb110743
Rune <sungjong5k@naver.com>
2025-07-07 14:05:23 +0900

    Refactor workspace and order handling in components

--- src/jotai/user/atoms.ts
+++ src/jotai/user/atoms.ts
@@ -1,10 +1,12 @@
 import { defaultUserOrderValue, defaultWorkspaceValue } from '@@types/defaultValues';
 import { Order, OrderProductBase, ProductCategory, Workspace } from '@@types/index';
 import { atom } from 'jotai';
-import { atomWithReset } from 'jotai/utils';
+import { atomWithReset, atomWithStorage } from 'jotai/utils';
 import { adminCategoriesAtom } from '../admin/atoms';
 
-export const userWorkspaceAtom = atom<Workspace>(defaultWorkspaceValue);
+export const userWorkspaceAtom = atomWithStorage<Workspace>('userWorkspace', defaultWorkspaceValue, undefined, {
+  getOnInit: true,
+});
 export const userOrderAtom = atom<Order>(defaultUserOrderValue);
 export const userOrderBasketAtom = atomWithReset<OrderProductBase[]>([]);
 export const userCategoriesAtom = atom<ProductCategory[]>((get) => get(adminCategoriesAtom));
