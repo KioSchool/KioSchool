90cd42d0af0c035816f1ce86ce09d3a940f7fc32
Rune <sungjong5k@naver.com>
2025-07-26 19:41:16 +0900

    Add styled layout TableCount component

--- src/components/admin/order/table-management/setting/TableCount.tsx
+++ src/components/admin/order/table-management/setting/TableCount.tsx
@@ -1,5 +1,36 @@
+import styled from '@emotion/styled';
+import { Color } from '@resources/colors';
+import { colFlex } from '@styles/flexStyles';
+
+const Container = styled.div`
+  width: 100%;
+  height: 150px;
+  border: 1px solid #ececec;
+  border-radius: 10px;
+  overflow: hidden;
+  ${colFlex({ justify: 'start', align: 'center' })}
+`;
+
+const Header = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 40px;
+  padding: 5px 10px;
+  color: ${Color.GREY};
+  background-color: ${Color.LIGHT_GREY};
+  font-size: 15px;
+  font-weight: 400;
+  border-bottom: 1px solid #ececec;
+  ${colFlex({ justify: 'center', align: 'center' })};
+`;
+
 function TableCount() {
-  return <div>TableCount</div>;
+  return (
+    <Container>
+      <Header>테이블 수</Header>
+      <div>TableCount</div>
+    </Container>
+  );
 }
 
 export default TableCount;
