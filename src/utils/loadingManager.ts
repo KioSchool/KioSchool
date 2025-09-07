dcc67109c4d332dd7dd4234e7ea43a4374ae3d10
Rune <sungjong5k@naver.com>
2025-09-07 18:02:21 +0900

    Refactor LoadingManager to use store directly for state

--- src/utils/loadingManager.ts
+++ src/utils/loadingManager.ts
@@ -2,27 +2,22 @@ import { getDefaultStore } from 'jotai';
 import { isLoadingAtom } from 'src/jotai/atoms';
 
 class LoadingManager {
-  private count = 0;
-
   private store = getDefaultStore();
 
   public increment(): void {
-    this.count++;
-    this.store.set(isLoadingAtom, this.count);
+    this.store.set(isLoadingAtom, (count) => count + 1);
   }
 
   public decrement(): void {
-    this.count = Math.max(0, this.count - 1);
-    this.store.set(isLoadingAtom, this.count);
+    this.store.set(isLoadingAtom, (count) => Math.max(0, count - 1));
   }
 
   public reset(): void {
-    this.count = 0;
-    this.store.set(isLoadingAtom, this.count);
+    this.store.set(isLoadingAtom, 0);
   }
 
   public getCount(): number {
-    return this.count;
+    return this.store.get(isLoadingAtom);
   }
 }
 
