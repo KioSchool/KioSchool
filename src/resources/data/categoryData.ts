493930243c90c7743791fe68dc84a3a67a5a708d
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-10-11 00:10:31 +0900

    Add default category data

---
+++ src/resources/data/categoryData.ts
@@ -0,0 +1,11 @@
+﻿import { ProductCategory } from '@@types/index';
+
+export const defaultCategoryName = '기본 메뉴';
+
+export const defaultCategory: ProductCategory = {
+  id: -1,
+  name: defaultCategoryName,
+  index: 0,
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+};
