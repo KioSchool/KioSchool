4fb221ad868fe8ed5e9b795a0615de1c8774c0ea
Rune <sungjong5k@naver.com>
2025-10-19 00:23:30 +0900

    Add font-family to navBarLabelStyle for consistency

--- src/styles/navBarStyles.tsx
+++ src/styles/navBarStyles.tsx
@@ -6,6 +6,7 @@ export const navBarLabelStyle = `
   font-weight: 400;
   text-decoration: none;
   color: ${Color.GREY};
+  font-family: 'LINE Seed Sans KR';
   cursor: pointer;
   transition: ease-in 0.1s;
   &:hover {
