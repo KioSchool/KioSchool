868b08894da38d65fd9130fbb65637d7cadd4a75
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-10-21 03:40:27 +0900

    Add common OrderCard styles

---
+++ src/styles/orderCardStyles.tsx
@@ -0,0 +1,60 @@
+﻿import styled from '@emotion/styled';
+import { colFlex, rowFlex } from '@styles/flexStyles';
+import { Color } from '@resources/colors';
+import { RiCheckboxCircleFill } from '@remixicon/react';
+import { expandButtonStyle } from '@styles/buttonStyles';
+
+export const CardContainer = styled.div<{ height: number }>`
+  ${colFlex({
+    justify: 'space-between',
+    align: 'start',
+  })}
+  background-color: ${Color.WHITE};
+  width: 150px;
+  height: ${(props) => props.height}px;
+  border-radius: 16px;
+  border: 1px solid #e8eef2;
+  padding: 12px 15px;
+
+  &:hover {
+    background-color: ${Color.KIO_ORANGE};
+
+    & * {
+      background-color: ${Color.KIO_ORANGE};
+      color: ${Color.WHITE} !important;
+      stroke: ${Color.WHITE};
+    }
+  }
+`;
+
+export const OrderInfoContainer = styled.div`
+  ${colFlex()}
+  box-sizing: border-box;
+  width: 100%;
+  cursor: pointer;
+  border-radius: 10px;
+`;
+
+export const TitleContainer = styled.div`
+  width: 100%;
+  ${rowFlex({ justify: 'space-between', align: 'center' })}
+`;
+
+export const DescriptionContainer = styled.div`
+  width: 100%;
+  height: 24px;
+  ${rowFlex({ justify: 'space-between', align: 'center' })}
+`;
+
+export const CheckButtonContainer = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  ${rowFlex({ justify: 'end', align: 'end' })}
+`;
+
+export const CheckIcon = styled(RiCheckboxCircleFill)`
+  width: 20px;
+  height: 20px;
+  color: ${Color.GREY};
+  ${expandButtonStyle}
+`;
