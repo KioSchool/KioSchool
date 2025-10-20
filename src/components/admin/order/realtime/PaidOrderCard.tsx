11b8f8dff2c1e2ac59a5d555c544519e0dfe3aae
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-10-21 03:43:10 +0900

    Refactor style of OrderCard components

--- src/components/admin/order/realtime/PaidOrderCard.tsx
+++ src/components/admin/order/realtime/PaidOrderCard.tsx
@@ -1,72 +1,13 @@
 import { memo } from 'react';
 import { Order } from '@@types/index';
 import AppLabel from '@components/common/label/AppLabel';
-import styled from '@emotion/styled';
-import { Color } from '@resources/colors';
-import { RiArrowRightSLine } from '@remixicon/react';
-import { expandButtonStyle } from '@styles/buttonStyles';
-import { colFlex, rowFlex } from '@styles/flexStyles';
 import OrderDetailModal from '@components/admin/order/realtime/modal/order-detail/OrderDetailModal';
 import OrderItemList from '@components/admin/order/realtime/OrderItemList';
 import { areOrdersEquivalent } from '@utils/MemoCompareFunction';
 import useModal from '@hooks/useModal';
 import { extractMinFromDate } from '@utils/FormatDate';
 import useFormattedTime from '@hooks/useFormattedTime';
-
-const CardContainer = styled.div`
-  ${colFlex({ justify: 'center', align: 'center' })}
-  background-color: #f4f4f4;
-  width: 200px;
-  height: 260px;
-  border-radius: 10px;
-
-  &:hover {
-    background-color: ${Color.KIO_ORANGE};
-
-    & * {
-      background-color: ${Color.KIO_ORANGE};
-      color: ${Color.WHITE};
-      stroke: ${Color.WHITE};
-    }
-  }
-`;
-
-const CardContents = styled.div`
-  ${colFlex({ justify: 'space-between', align: 'center' })}
-  width: 100%;
-  height: 90%;
-`;
-
-const OrderInfoContainer = styled.div`
-  ${colFlex()}
-  box-sizing: border-box;
-  padding: 5px 15px;
-  width: 100%;
-  border-radius: 10px;
-  cursor: pointer;
-`;
-
-const HeaderContainer = styled.div`
-  ${rowFlex({ justify: 'space-between', align: 'start' })}
-  width: 100%;
-`;
-
-const TitleContainer = styled.div`
-  ${colFlex({ align: 'start' })}
-  padding-top: 3px;
-  gap: 5px;
-`;
-
-const HorizontalLine = styled.hr`
-  width: 100%;
-  border: 0.3px solid #d9d9d9;
-`;
-
-const RightIcon = styled(RiArrowRightSLine)`
-  width: 25px;
-  height: 25px;
-  ${expandButtonStyle}
-`;
+import { CardContainer, CheckButtonContainer, CheckIcon, DescriptionContainer, OrderInfoContainer } from '@styles/orderCardStyles';
 
 const arePropsEqual = (prevProps: OrderCardProps, nextProps: OrderCardProps) => {
   return areOrdersEquivalent(prevProps.order, nextProps.order);
@@ -85,26 +26,22 @@ function PaidOrderCard({ order }: OrderCardProps) {
   };
 
   return (
-    <>
-      <CardContainer>
-        <CardContents>
-          <OrderInfoContainer onClick={orderInfoClickHandler}>
-            <HeaderContainer>
-              <TitleContainer>
-                <AppLabel size={17} style={{ fontWeight: 800 }}>
-                  {`테이블 ${order.tableNumber}`}
-                </AppLabel>
-                <AppLabel size={13}>{`${delayMinutes}분 전`}</AppLabel>
-              </TitleContainer>
-              <RightIcon onClick={openModal} />
-            </HeaderContainer>
-            <HorizontalLine />
-            <OrderItemList order={order} />
-          </OrderInfoContainer>
-          <OrderDetailModal order={order} isModalOpen={isModalOpen} closeModal={closeModal} />
-        </CardContents>
-      </CardContainer>
-    </>
+    <CardContainer height={234} onClick={openModal}>
+      <OrderInfoContainer onClick={orderInfoClickHandler}>
+        <AppLabel size={16} style={{ fontWeight: 800, color: '#464A4D' }}>
+          {order.customerName}
+        </AppLabel>
+        <DescriptionContainer>
+          <AppLabel size={12} style={{ fontWeight: 800, color: '#464A4D' }}>{`${delayMinutes}분 전`}</AppLabel>
+          <AppLabel size={12} style={{ fontWeight: 800, color: '#464A4D' }}>{`총 ${order.totalPrice.toLocaleString()}원`}</AppLabel>
+        </DescriptionContainer>
+      </OrderInfoContainer>
+      <OrderItemList order={order} />
+      <CheckButtonContainer>
+        <CheckIcon onClick={openModal} />
+      </CheckButtonContainer>
+      <OrderDetailModal order={order} isModalOpen={isModalOpen} closeModal={closeModal} />
+    </CardContainer>
   );
 }
 
