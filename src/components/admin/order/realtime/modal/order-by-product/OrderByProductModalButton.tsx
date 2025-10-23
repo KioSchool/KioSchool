113e7d3da6dbe2ac212c732b5936634b93b39a82
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-10-23 21:09:08 +0900

    Refactor OrderByProducModalButton styles

--- src/components/admin/order/realtime/modal/order-by-product/OrderByProductModalButton.tsx
+++ src/components/admin/order/realtime/modal/order-by-product/OrderByProductModalButton.tsx
@@ -1,76 +1,45 @@
 import { colFlex } from '@styles/flexStyles';
 import styled from '@emotion/styled';
-import { Color } from '@resources/colors';
 import { expandButtonStyle } from '@styles/buttonStyles';
 import { tabletMediaQuery } from '@styles/globalStyles';
-import { RiArrowRightSLine } from '@remixicon/react';
+import { RiArrowLeftSLine } from '@remixicon/react';
 
-const ModalContainer = styled.div`
-  ${colFlex({ justify: 'space-evenly', align: 'center' })};
+const ModalButtonContainer = styled.div`
   position: fixed;
-  top: calc(50% - 50px);
+  top: calc(50% - 25px);
   right: 20px;
-  color: ${Color.WHITE};
-  background-color: ${Color.BLACK};
+  color: #464a4d;
   border-radius: 50%;
-  width: 100px;
-  height: 100px;
+  width: 50px;
+  height: 50px;
   cursor: pointer;
   z-index: 1998;
   box-sizing: border-box;
-  padding: 10px;
+  ${colFlex({ justify: 'center', align: 'center' })};
   ${expandButtonStyle}
 
   ${tabletMediaQuery} {
-    width: 80px;
-    height: 80px;
+    width: 44px;
+    height: 44px;
     right: 20px;
+    top: calc(50% - 22px);
   }
 `;
 
-const TextContainer = styled.div`
-  ${colFlex({ justify: 'center', align: 'center' })};
-  width: 100%;
-  height: 30px;
-`;
-
-const Text = styled.div`
-  font-size: 14px;
-  font-weight: bold;
-  white-space: pre-wrap;
-  text-align: center;
-`;
-
-const Icon = styled(RiArrowRightSLine, {
-  shouldForwardProp: (prop) => prop !== 'isModalOpen',
-})<{ isModalOpen: boolean }>`
-  transform: ${({ isModalOpen }) => (isModalOpen ? 'rotate(0deg)' : 'rotate(180deg)')};
-`;
-
 interface OrderByProductModalButtonProps {
-  isModalOpen: boolean;
   openModal: () => void;
-  closeModal: () => void;
 }
 
-function OrderByProductModal({ isModalOpen, openModal, closeModal }: OrderByProductModalButtonProps) {
+function OrderByProductModalButton({ openModal }: OrderByProductModalButtonProps) {
   const onClick = () => {
-    if (isModalOpen) {
-      closeModal();
-    } else {
-      openModal();
-    }
+    openModal();
   };
-  const text = isModalOpen ? '닫기' : '상품별\n주문 보기';
 
   return (
-    <ModalContainer onClick={onClick}>
-      <TextContainer>
-        <Text>{text}</Text>
-      </TextContainer>
-      <Icon isModalOpen={isModalOpen} />
-    </ModalContainer>
+    <ModalButtonContainer onClick={onClick}>
+      <RiArrowLeftSLine size={35} />
+    </ModalButtonContainer>
   );
 }
 
-export default OrderByProductModal;
+export default OrderByProductModalButton;
