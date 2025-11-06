3a7ca97cbdfe267d276ba6c6d1ae589bcc19c6b3
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-07 01:56:37 +0900

    Edit TossAccountInfo component design

--- src/components/admin/account/RegisterAccountInfoContainer.tsx
+++ src/components/admin/account/RegisterAccountInfoContainer.tsx
@@ -3,14 +3,19 @@ import styled from '@emotion/styled';
 import { colFlex, rowFlex } from '@styles/flexStyles';
 
 const Container = styled.div`
-  width: 594px;
+  // TODO : 디자인이 생각보다 너무 넓어서 자체적으로 줄임
+  width: 50%;
   height: 100%;
-  padding: 30px 18px;
+  padding: 18px 30px;
   box-sizing: border-box;
-  ${colFlex({ justify: 'center', align: 'center' })}
+  border: 1px solid #e8eef2;
+  border-radius: 16px;
+  box-shadow: 0 4px 20px 0 rgba(92, 92, 92, 0.05);
+  ${colFlex({ justify: 'start', align: 'center' })}
 `;
 
 const Title = styled.span`
+  width: 100%;
   font-size: 16px;
   font-weight: bold;
   color: #464a4d;
@@ -18,12 +23,14 @@ const Title = styled.span`
 
 const ContentWrapper = styled.div`
   width: 100%;
-  ${colFlex({ justify: 'flex-start', align: 'stretch' })}
+  height: 100%;
+  ${colFlex({ justify: 'center', align: 'center' })}
 `;
 
 const ButtonWrapper = styled.div`
   width: 360px;
   gap: 16px;
+  padding-bottom: 20px;
   ${rowFlex({ justify: 'center', align: 'center' })}
 `;
 
--- src/components/admin/account/TossAccountInfo.tsx
+++ src/components/admin/account/TossAccountInfo.tsx
@@ -1,31 +1,37 @@
-import styled from '@emotion/styled';
-import { Color } from '@resources/colors';
-import { colFlex } from '@styles/flexStyles';
-import AppLabel from '@components/common/label/AppLabel';
 import { adminUserTossAccountAtom } from 'src/jotai/admin/atoms';
 import { useAtomValue } from 'jotai';
-
-const Container = styled.div`
-  width: 30%;
-  height: 100%;
-  ${colFlex({ justify: 'space-between', align: 'center' })};
-`;
-
-const Title = styled.div`
-  color: ${Color.BLACK};
-  font-size: 15px;
-  font-weight: 500;
-`;
+import RegisterAccountInfoContainer from './RegisterAccountInfoContainer';
+import RegistrationStatusInfo from './RegistrationStatusInfo';
 
 function TossAccountInfo() {
   const tossAccountInfo = useAtomValue(adminUserTossAccountAtom);
-  const text = tossAccountInfo ? '등록됨 ✅' : '미등록 ❌';
+  const status = tossAccountInfo ? 'registered' : 'unregistered';
+
+  const handleRegisterQr = () => {
+    // TODO: QR 등록 모달 열기 또는 관련 로직 실행
+    console.log('QR 등록 클릭');
+  };
+
+  const handleDeleteQr = () => {
+    // TODO: QR 삭제 확인 또는 관련 로직 실행
+    console.log('QR 삭제 클릭');
+  };
 
   return (
-    <Container>
-      <Title>토스 QR 등록 여부</Title>
-      <AppLabel size={15}>{text}</AppLabel>
-    </Container>
+    <RegisterAccountInfoContainer
+      title="등록된 토스 QR"
+      secondaryButton={{
+        text: 'QR 삭제',
+        onClick: handleDeleteQr,
+        disabled: !tossAccountInfo,
+      }}
+      primaryButton={{
+        text: 'QR 등록',
+        onClick: handleRegisterQr,
+      }}
+    >
+      <RegistrationStatusInfo status={status} />
+    </RegisterAccountInfoContainer>
   );
 }
 
