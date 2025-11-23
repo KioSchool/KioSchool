4d1badaeaa8a20ace51c04ddd97379af4c4e6efe
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-24 06:45:56 +0900

    Refactor ProductCard styles

--- src/components/admin/product/ProductCard.tsx
+++ src/components/admin/product/ProductCard.tsx
@@ -1,9 +1,8 @@
 import { Product } from '@@types/index';
 import styled from '@emotion/styled';
-import SwitchButton from '@components/common/button/SwitchButton';
-import useAdminProducts from '@hooks/admin/useAdminProducts';
-import { useParams } from 'react-router-dom';
-import { colFlex, rowFlex } from '@styles/flexStyles';
+// import useAdminProducts from '@hooks/admin/useAdminProducts';
+// import { useParams } from 'react-router-dom';
+import { colFlex } from '@styles/flexStyles';
 import { Color } from '@resources/colors';
 
 const sellableStyle = `
@@ -15,63 +14,47 @@ const unSellableStyle = `
 `;
 
 const Container = styled.div<{ isSellable: boolean | null }>`
-  width: 220px;
-  height: 320px;
-  padding: 25px 37px;
-  border-radius: 13px;
+  width: 180px;
+  height: 228px;
+  padding: 16px;
+  border-radius: 16px;
+  border: 1px solid #e8eef2;
   box-sizing: border-box;
   ${(props) => (props.isSellable ? sellableStyle : unSellableStyle)}
-  ${colFlex({ justify: 'center', align: 'center' })}
-`;
-
-const SubContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  gap: 15px;
-  ${colFlex({ justify: 'center', align: 'center' })};
+  ${colFlex({ justify: 'start', align: 'center' })}
 `;
 
 const Title = styled.div`
-  font-size: 20px;
+  color: #464a4d;
+  line-height: 24px;
+  font-size: 16px;
   font-weight: 700;
 `;
 
 const Price = styled.div`
-  font-size: 20px;
+  color: #464a4d;
+  line-height: 24px;
+  font-size: 12px;
   font-weight: 500;
 `;
 
 const ContentContainer = styled.div`
   cursor: pointer;
-  gap: 15px;
   ${colFlex({ justify: 'center', align: 'center' })};
 `;
 
 const TextContainer = styled.div<{ isSellable: boolean | null }>`
-  height: 80px;
   color: ${(props) => (props.isSellable ? Color.BLACK : '#B2B2B2')};
+  padding-bottom: 12px;
   ${colFlex({ justify: 'center', align: 'center' })}
 `;
 
-const ImageContainer = styled.div<{ isSellable: boolean | null }>`
-  width: 150px;
-  height: 120px;
-  filter: ${(props) => (props.isSellable ? 'none' : 'grayScale(50%)')};
-  ${rowFlex({ justify: 'center', align: 'center' })}
-`;
-
 const Image = styled.img<{ isSellable: boolean | null }>`
-  width: 125px;
-  height: 125px;
-  border-radius: 5px;
-  border: 0.5px solid #e8e8e8;
+  width: 100px;
+  height: 100px;
+  border-radius: 6px;
+  border: 1px solid #e8eef2;
   opacity: ${(props) => (props.isSellable ? 1 : 0.2)};
-`;
-
-const SwitchButtonContainer = styled.div`
-  width: 100%;
-  height: 40px;
-  ${rowFlex({ justify: 'center', align: 'center' })}
 `;
 
 interface Props {
@@ -80,32 +63,28 @@ interface Props {
 }
 
 function ProductCard({ product, onClick }: Props) {
-  const { workspaceId } = useParams<{ workspaceId: string }>();
-  const { editProductSellable } = useAdminProducts(workspaceId);
+  // const { workspaceId } = useParams<{ workspaceId: string }>();
+  // const { editProductSellable } = useAdminProducts(workspaceId);
 
   return (
     <Container isSellable={product.isSellable} className={'product-card-container'}>
-      <SubContainer className={'product-card-sub-container'}>
-        <ContentContainer onClick={onClick} className={'content-container'}>
-          <TextContainer isSellable={product.isSellable} className={'text-container'}>
-            <Title>{product.name}</Title>
-            <Price>{product.price.toLocaleString()}원</Price>
-          </TextContainer>
-          <ImageContainer isSellable={product.isSellable} className={'image-container'}>
-            <Image isSellable={product.isSellable} src={product.imageUrl} alt={product.name} />
-          </ImageContainer>
-        </ContentContainer>
-        <SwitchButtonContainer>
-          <SwitchButton
-            checked={!!product.isSellable}
-            onChange={() => {
-              editProductSellable(product.id, !product.isSellable);
-            }}
-            checkedText="SHOW"
-            uncheckedText="HIDE"
-          />
-        </SwitchButtonContainer>
-      </SubContainer>
+      <ContentContainer onClick={onClick} className={'content-container'}>
+        <TextContainer isSellable={product.isSellable} className={'text-container'}>
+          <Title>{product.name}</Title>
+          <Price>{product.price.toLocaleString()}원</Price>
+        </TextContainer>
+        <Image isSellable={product.isSellable} src={product.imageUrl} alt={product.name} />
+      </ContentContainer>
+      {/* <SwitchButtonContainer>
+        <SwitchButton
+          checked={!!product.isSellable}
+          onChange={() => {
+            editProductSellable(product.id, !product.isSellable);
+          }}
+          checkedText="SHOW"
+          uncheckedText="HIDE"
+        />
+      </SwitchButtonContainer> */}
     </Container>
   );
 }
