9dc5376db9272f6c97a750747d21e19a9f084080
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-12-10 16:50:50 +0900

    Add new status option to constants of product

--- src/constants/data/productData.ts
+++ src/constants/data/productData.ts
@@ -7,9 +7,9 @@ interface StatusOption {
 }
 
 export const STATUS_OPTIONS: StatusOption[] = [
-  { label: '판매 중', value: 'SELLING', color: '#ff9142' },
-  { label: '품절', value: 'SOLD_OUT', color: '#5c5c5c' },
-  { label: '숨김', value: 'HIDDEN', color: '#e8eef2' },
+  { label: '판매 중', value: ProductStatus.SELLING, color: '#ff9142' },
+  { label: '품절', value: ProductStatus.SOLD_OUT, color: '#5c5c5c' },
+  { label: '숨김', value: ProductStatus.HIDDEN, color: '#e8eef2' },
 ];
 
 export const MAX_PRODUCT_IMAGE_SIZE = 1024 * 1024 * 5;
