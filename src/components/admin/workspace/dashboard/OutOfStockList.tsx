520e3ee2dfc4e6a54883e0c42339c4b9d3cf9e35
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2026-02-09 00:08:12 +0900

    Add action button to OutOfStockList component

--- src/components/admin/workspace/dashboard/OutOfStockList.tsx
+++ src/components/admin/workspace/dashboard/OutOfStockList.tsx
@@ -3,9 +3,11 @@ import { match } from 'ts-pattern';
 import DashboardCard from './DashboardCard';
 import ProductCard from '@components/admin/product/ProductCard';
 import { rowFlex } from '@styles/flexStyles';
-import { EmptyText } from '@styles/dashboardStyles';
+import { ActionButton, EmptyText } from '@styles/dashboardStyles';
 import { useAtomValue } from 'jotai';
 import { adminDashboardAtom } from '@jotai/admin/atoms';
+import { useNavigate } from 'react-router-dom';
+import { ADMIN_ROUTES } from '@constants/routes';
 
 const ProductList = styled.div`
   overflow-x: auto;
@@ -21,10 +23,16 @@ const ScaledProductWrapper = styled.div`
 `;
 
 function OutOfStockList() {
+  const navigate = useNavigate();
   const { outOfStockProducts } = useAtomValue(adminDashboardAtom);
 
+  const handleNavigate = () => {
+    navigate(ADMIN_ROUTES.PRODUCTS);
+  };
+
+  const rightAction = <ActionButton onClick={handleNavigate}>상품 관리 페이지로 이동 {'>'}</ActionButton>;
   return (
-    <DashboardCard title="품절된 상품" width={480} height={240} showDivider={false}>
+    <DashboardCard title="품절된 상품" width={480} height={240} rightAction={rightAction} showDivider={false}>
       {match(outOfStockProducts.length)
         .with(0, () => <EmptyText>품절된 상품이 없습니다.</EmptyText>)
         .otherwise(() => (
--- src/styles/dashboardStyles.ts
+++ src/styles/dashboardStyles.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Color } from '@resources/colors';
 
 export const EmptyText = styled.div`
   line-height: 24px;
@@ -9,6 +10,19 @@ export const EmptyText = styled.div`
 export const DashboardCardDivider = styled.div`
   border-top: 1px solid #e8eef2;
   width: 100%;
+`;
+
+export const ActionButton = styled.button`
+  font-size: 10px;
+  color: #464a4d;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+
+  &:hover {
+    color: ${Color.KIO_ORANGE};
+  }
 `;
 
 export const getRankBackgroundColor = (rank: number): string => {
