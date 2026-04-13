6290ba076e16fbc9758e0920559e50b6b25378dc
Rune <sungjong5k@naver.com>
2026-04-13 21:06:15 +0900

    refactor: 키오스쿨 가이드 유튜브 toast를 어드민 홈으로 이동
    랜딩 페이지는 서비스 소개 용도로 단순화하고, 어드민 가입 유도 가이드는
    실제 운영을 시작하는 어드민 홈에서 노출되도록 위치를 조정.
    
    - toast 띄우는 로직을 Home → AdminHome으로 이동
    - KioSchoolGuideYoutubeContent 파일을 components/user/home에서
      components/admin/home으로 이동 (사용처에 맞춰 재배치)

--- src/pages/user/home/Home.tsx
+++ src/pages/user/home/Home.tsx
@@ -1,12 +1,8 @@
-import { useEffect } from 'react';
 import styled from '@emotion/styled';
-import { toast } from 'react-toastify';
-import { useCookies } from 'react-cookie';
 import { MotionConfig } from 'framer-motion';
 import AppContainer from '@components/common/container/AppContainer';
 import AppFaqButton from '@components/common/button/AppFaqButton';
 import { colFlex } from '@styles/flexStyles';
-import KioSchoolGuideYoutubeContent, { KIOSCHOOL_GUIDE_YOUTUBE_TOAST_COOKIE } from '@components/user/home/KioSchoolGuideYoutubeContent';
 import HeroSection from '@components/user/home/hero/HeroSection';
 import PainPointSection from '@components/user/home/PainPointSection';
 import HowItWorksSection from '@components/user/home/how-it-works/HowItWorksSection';
@@ -25,29 +21,7 @@ const LandingWrapper = styled.div`
   }
 `;
 
-const YOUTUBE_TOAST_ID = 'youtube-guide-toast';
-
 function Home() {
-  const [cookies] = useCookies([KIOSCHOOL_GUIDE_YOUTUBE_TOAST_COOKIE]);
-
-  useEffect(() => {
-    if (!cookies[KIOSCHOOL_GUIDE_YOUTUBE_TOAST_COOKIE]) {
-      toast(<KioSchoolGuideYoutubeContent onDismiss={() => toast.dismiss(YOUTUBE_TOAST_ID)} />, {
-        toastId: YOUTUBE_TOAST_ID,
-        position: 'top-right',
-        autoClose: false,
-        hideProgressBar: true,
-        closeOnClick: false,
-        draggable: true,
-        closeButton: true,
-      });
-    }
-
-    return () => {
-      toast.dismiss(YOUTUBE_TOAST_ID);
-    };
-  }, [cookies]);
-
   return (
     <MotionConfig reducedMotion="user">
       <LandingWrapper>
