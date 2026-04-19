ea9e718cb6ddb8e26b6aaf252c3bbd96d088042c
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2026-04-19 18:44:36 +0900

    Add custom props to AppBadge Component

--- src/components/common/badge/AppBadge.tsx
+++ src/components/common/badge/AppBadge.tsx
@@ -1,24 +1,78 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { ReactNode } from 'react';
 import { Color } from '@resources/colors';
+import { rowFlex } from '@styles/flexStyles';
+
+export interface CustomBadgeSize {
+  height?: number;
+  font?: number;
+  borderRadius?: number;
+  paddingX?: number;
+}
+
+export interface CustomBadgeTheme {
+  background?: string;
+  color?: string;
+  border?: string;
+}
 
 interface AppBadgeProps extends React.HTMLAttributes<HTMLDivElement> {
   background?: string;
   noBorder?: boolean;
+  customSize?: CustomBadgeSize;
+  customColors?: CustomBadgeTheme;
+  icon?: ReactNode;
+  gap?: number;
 }
 
-const Container = styled.div`
+const sizeStyles = (customSize: CustomBadgeSize | undefined) => {
+  if (!customSize) {
+    return css`
+      padding: 0 10px;
+      height: 30px;
+      border-radius: 1000px;
+      line-height: 30px;
+      font-size: 15px;
+    `;
+  }
+
+  return css`
+    padding: 0 ${customSize.paddingX ?? 10}px;
+    height: ${customSize.height ?? 30}px;
+    border-radius: ${customSize.borderRadius ?? 1000}px;
+    line-height: ${customSize.height ?? 30}px;
+    font-size: ${customSize.font ?? 15}px;
+  `;
+};
+
+const colorStyles = (props: AppBadgeProps) => {
+  if (props.customColors) {
+    return css`
+      background: ${props.customColors.background ?? props.background ?? Color.WHITE};
+      color: ${props.customColors.color ?? Color.BLACK};
+      border: ${props.customColors.border ?? (props.noBorder ? 'none' : '1px solid black')};
+    `;
+  }
+
+  return css`
+    background: ${props.background || Color.WHITE};
+    color: ${Color.BLACK};
+    border: ${props.noBorder ? 'none' : '1px solid black'};
+  `;
+};
+
+const Container = styled.div<AppBadgeProps>`
   width: auto;
-  padding: 0 10px;
-  height: 30px;
-  border: ${(props: AppBadgeProps) => (props.noBorder ? 'none' : '1px solid black')};
-  border-radius: 1000px;
   box-sizing: border-box;
   text-align: center;
-  line-height: 30px;
-  font-size: 15px;
-  background: ${(props: AppBadgeProps) => props.background || Color.WHITE};
   user-select: none;
   white-space: nowrap;
+  gap: ${({ gap }) => gap ?? 4}px;
+  ${rowFlex({ justify: 'center', align: 'center' })}
+
+  ${({ customSize }) => sizeStyles(customSize)}
+  ${(props) => colorStyles(props)}
 
   :active {
     background-color: gray;
@@ -29,7 +83,14 @@ const Container = styled.div`
   }
 `;
 function AppBadge(props: AppBadgeProps) {
-  return <Container {...props} className={'app-badge'} />;
+  const { icon, children, ...rest } = props;
+
+  return (
+    <Container {...rest} className={'app-badge'}>
+      {icon}
+      {children}
+    </Container>
+  );
 }
 
 export default AppBadge;
