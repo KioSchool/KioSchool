379ff1e8f4b5de4740ccf28dd2f7e906f890a4af
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2026-04-29 00:48:42 +0900

    Fix prettier

--- src/jotai/admin/atoms.ts
+++ src/jotai/admin/atoms.ts
@@ -1,5 +1,16 @@
 import { defaultBanksValue, defaultDashboardValue, defaultUserValue, defaultWorkspaceValue } from '@@types/defaultValues';
-import { Bank, DashboardResponse, ExternalRightSidebarOptions, Order, Product, ProductCategory, RIGHT_SIDEBAR_ACTION, Table, User, Workspace } from '@@types/index';
+import {
+  Bank,
+  DashboardResponse,
+  ExternalRightSidebarOptions,
+  Order,
+  Product,
+  ProductCategory,
+  RIGHT_SIDEBAR_ACTION,
+  Table,
+  User,
+  Workspace,
+} from '@@types/index';
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
