a25d83d810fae08a66484bb620739c96708c32b9
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2026-05-12 16:00:08 +0900

    feat(build): prerender marketing routes and add sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 /* eslint-disable import/no-extraneous-dependencies */
+import * as path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    ...vitePrerenderPlugin({
+      renderTarget: '#root',
+      prerenderScript: path.resolve(__dirname, 'src/prerender.tsx'),
+      additionalPrerenderRoutes: ['/info'],
+    }),
     sentryVitePlugin({
       org: 'kioschool',
       project: 'kio-school',
